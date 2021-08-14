1e2157e0512c52a5736fe66480445fcec4ac69a9
Bennett Lin <bennettslin@gmail.com>
2021-08-13 23:30:55 -0700

    More about content.

--- src/content/about/index.js
+++ src/content/about/index.js
@@ -1,38 +1,35 @@
 export default `
 ### So epic it couldn't fail
 
-A long time ago, I was in a band with some friends of mine. We'd been playing out for several years, and I was frustrated that my friends didn't share my ambition to make it big. Yet I struggled to find ambitious bandmates elsewhere, because I didn't resemble any front person who'd ever once made it.
+A long time ago, I was in a band with some friends of mine. We'd been playing out for several years, and I was frustrated that my friends didn't share my ambition to make it big. Yet I struggled to find ambitious bandmates elsewhere, because I didn't resemble any front person who'd ever made it.
 
 Back then, I was studying masterpieces like James Joyce's *Ulysses* and the Beatles' later albums, hoping to learn what makes a work great. As I read about the extensive recording sessions for *Sgt. Pepper's*, I realized something. With home studios becoming more affordable, a band no longer needed to be established to record their first masterpiece.
 
-And then it dawned on me. I should create a masterpiece, "The *Ulysses* of Rock Albums," on my own. It would be so epic that I couldn't fail to make it big. I assumed that others out there were thinking similarly, and I would emerge to find myself part of an exciting wave of new artists, each of us striving to be as great as the Beatles.
+And then it dawned on me. I should create a masterpiece, "The *Ulysses* of Rock Albums," on my own. It would be so epic that I couldn't fail to make it big. I assumed that others out there were thinking similarly, and I would emerge to find myself part of an exciting wave of new bands, each striving to be as great as the Beatles.
 
-### A Dunning-Kruger masterpiece
+### A Dunning-Kruger artist
 
 But there was no new wave. Instead, upon completion, every demo of *Yearling's Bobtail* was rejected. I soon understood why. With so many unknown bands hoping to be considered, the gatekeepers can only give each release the same bare minimum of their time. Want another chance? Put out another release.
 
-Still, what's hard for one is hard for all. And so the most promising bands embrace the challenge by putting out frequent releases. This maximizes their chances while giving them continual feedback for improvement.
+Still, what's hard for one is hard for all. And so the most promising bands embrace this challenge, tailoring their creative approach to put out frequent releases. This maximizes their chances while giving them continual feedback for improvement.
 
-But *Yearling's Bobtail* took more than five years to create. It was designed to reveal hidden layers of meaning upon repeated listens. Yet there was no way for me to signal this. If it were possible, then every other band would do the same. So I just kept trying to get the same work heard, over and over— which is what you’d expect the *least* competent artists to do.
+But *Yearling's Bobtail* took more than five years to create. It was designed to reveal hidden layers of meaning upon repeated listens. And there was no way for me to signal this. If it were possible, then every other band would do the same. So I just kept trying to get the same work heard, over and over— which is exactly what the *least* competent artists are expected to do.
 
-In other words, any artist who creates “The *Ulysses* of Rock Albums” will get lumped in with those who suffer from the Dunning-Kruger effect. They will seem too incompetent to even realize it. And this got me excited. Because along with studying masterpieces, I'd also studied artistic revolutions. And every major revolution starts with the incompetent.
+In other words, any artist who creates “The *Ulysses* of Rock Albums” will get lumped in with those who suffer from the Dunning-Kruger effect. They will seem too incompetent to even realize it. But this actually got me excited. Because along with studying masterpieces, I'd also studied artistic revolutions. And every major revolution starts with the incompetent.
 
-### Building something from nothing
+### Building from nothing
 
-Every now and then, a respected cohort of underground bands is welcomed into the mainstream. This is a minor revolution. There is no momentum, because each incremental shifting of the needle faces new resistance. The next cohort is now the one whose music is too raw, or too experimental, for mainstream audiences.
+Every now and then, a respected cohort of underground bands is welcomed into the mainstream. This is a minor revolution. There is no momentum, because each incremental shift of the needle faces new resistance. The next cohort is now the one whose music is too raw, or too experimental, for a mainstream audience.
 
-A major revolution starts with those we dismiss as incompetent, like the early pioneers of jazz or hip hop. As these pioneers expand our understanding of competence, others who were shut out before can now join, expanding our understanding further. By building something from nothing in this virtuous circle, a major revolution gains the kind of momentum that makes it seem inevitable in hindsight.
+But a major revolution starts with those we see as incompetent, like the early pioneers of jazz or hip hop. Once they compel us to revise our notion of competence, others who were shut out before can now join, expanding the boundaries further. And so the revolution gains momentum by building outward from nothing in a virtuous circle, making it seem inevitable in hindsight.
 
-This phenomenon isn't limited to art. Henry Ford and Steve Jobs were dismissed as incompetent for trying to sell a worse car and a worse computer, respectively. But worse products are also cheaper, which allowed them to build something from nothing— namely, a middle class that could now join the market for cars and computers.
+This phenomenon isn't limited to art. Henry Ford and Steve Jobs were derided as incompetent for trying to sell a worse car and a worse computer, respectively. But worse products are also cheaper, and this allowed them to build something from nothing— namely, a middle class that could now join the market for cars and computers.
 
 ### The masterpiece revolution
 
-it's not hard to imagine a virtuous circle, where bands to spend more time and work harder to merit greater consideration for the same work. And yet, if they have to go through the same wringer I did, my masterpiece revolution begins and ends with me.
+It's not hard to imagine Yearling's Bobtail leading to a virtuous circle, where bands spend more time and work harder to merit greater consideration for the same work. And yet, if each of these bands has to go through the same wringer that I did, then this masterpiece revolution will begin and end with me. BYCombo seeks to prevent that.
 
-BYCombo is
+Gatekeepers will always be egalitarian. Only more time spent can ever be respected as a metric to signal that a work deserves further consideration.
 
-egalitarian
-
-Bring together 
-
+Bring together artists committed to spending the time to create a masterpiece. They can enforce a culture of a strong work ethic amongst themselves.
 `
