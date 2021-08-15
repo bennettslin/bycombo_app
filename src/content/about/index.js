82dbe41b224856a8ae1ba1c8f783e57e05041e08
Bennett Lin <bennettslin@gmail.com>
2021-08-15 15:16:26 -0700

    Even more about content.

--- src/content/about/index.js
+++ src/content/about/index.js
@@ -11,33 +11,29 @@ And then it hit me. I should create a masterpiece, "The *Ulysses* of Rock Albums
 
 But there was no new wave. Instead, upon completion, every demo of *Yearling's Bobtail* was rejected. I soon understood why. With so many unknown bands hoping to be considered, the gatekeepers can only grant each release a fleeting first impression. Want another chance? Release another work.
 
-Still, what's hard for one is hard for all. And so the most promising bands embrace the system, maximizing their chances by putting out more and frequent releases.
+Still, what's hard for one is hard for all. And so the most promising bands simply embrace the system, maximizing their chances by putting out more and frequent releases.
 
-But *Yearling's Bobtail* took me five years to create. It hides layers of meaning that are revealed upon repeated listens. And there was no way to signal this. If it were possible, then every other band would do the same. So I just kept trying to get the same work heard, again and again. But this is what the *least* competent artists do— because they don't understand how rejection can be used to get better.
+But *Yearling's Bobtail* took me five years to create. It hides layers of meaning that are revealed upon repeated listens. And there was no way to signal this. If it were possible, then every other band would do the same. So I just kept trying to get the same work heard, again and again. But this is what the *least* competent artists do— because they don't understand that rejection can be used to get better.
 
 An artist who creates “The *Ulysses* of Rock Albums,” in other words, will seem to exemplify the Dunning-Kruger effect. They will appear too incompetent to even recognize their own incompetence.
 
-### Building from nothing
+### Momentum from nothing
 
-Far from discouraging me, this got me excited. Because along with studying masterpieces, I'd also studied musical revolutions. And revolution starts with the incompetent.
+But far from discouraging me, this got me excited. Because along with studying masterpieces, I'd also studied musical revolutions. And revolution starts with the incompetent.
 
 Every now and then, some underground bands we respect are welcomed into the mainstream. Let's call this a minor revolution. There is no momentum, because each incremental shifting of the needle faces new resistance. The next cohort is now the one whose music is too raw, or too experimental, for a mainstream audience.
 
-But a major revolution starts with those we deem incompetent, like the early pioneers of jazz or hip hop. As they compel us to revise our notion of competence, others who were shut out can now join, pushing the boundary further to let even more in. And so, starting from nothing, the revolution builds up strength in a virtuous circle, making it seem inevitable in hindsight.
+But a major revolution starts with those we deem incompetent, like the early pioneers of jazz or hip hop. As they compel us to expand our notion of competence, others who were shut out can now join, pushing the boundary further to let even more in. And so, starting from nothing, the revolution builds momentum in a virtuous circle, making it seem inevitable in hindsight.
 
 This phenomenon isn't limited to art. Henry Ford and Steve Jobs were derided as incompetent for trying to sell a worse car and worse computer, respectively. But their products were also cheaper, which allowed them to build something from nothing— namely, a middle-class market for cars and computers.
 
 ### The masterpiece revolution
 
-It's not hard to imagine *Yearling's Bobtail* inspiring unknown bands to create their masterpieces here and now. These works could then inspire others, continuing a virtuous circle. But unless these bands can avoid the runaround I was given, this masterpiece revolution will begin and end with me. (Upon hearing Yearling's Bobtail.)
+It's not hard to imagine an unknown band hearing *Yearling's Bobtail* and being inspired to create their own masterpiece here and now. This work might then inspire others, continuing a virtuous circle. But unless these bands can avoid the runaround I was given, the masterpiece revolution will begin and end with me.
 
-The good news is that no one ever declared me incompetent. The system simply wrote me off as such. Which means this might be the first revolution in music history that wouldn't need hearts or minds to change. The problem is purely systemic, so its solution can be likewise.
+The good news is that no one ever accused me of incompetence. I was simply undone by a system that's been streamlined to weed it out. Which means this might be the first revolution in music history that doesn't need to change hearts or minds. The problem is merely systemic. Its solution can be likewise.
 
-To start with, the gatekeepers will always strive to be egalitarian, and "one chance per release" is as fair as it gets. 
-
-Only more time spent can ever be respected as a metric to signal that a work deserves further consideration.
+To start with, let's admit that "one release, one chance" is pretty fair. And so an alternative system must be no less egalitarian. I propose counting the number of hours that went into creating a work. By this metric, it would be perfectly fair for a masterpiece to be granted further consideration. BYCombo can verify this number for each of its bands, and help convince the gatekeepers of its value.
 
 Bring together artists committed to spending the time to create a masterpiece. They can enforce a culture of a strong work ethic amongst themselves.
 `
-
-// Still, what's hard for one is hard for all. And so the most promising bands embrace the system, tailoring their creative approach to put out more and frequent releases. This maximizes their chances while giving them continual feedback to do better next time.
