3d9d1eb6c5303fe52485bf5280302ab3d79b1499
Bennett Lin <bennettslin@gmail.com>
2021-08-18 00:15:12 -0700

    Modify home page content.

--- src/content/home/index.js
+++ src/content/home/index.js
@@ -2,7 +2,9 @@ export const title = `Hi, we're BYCombo`
 export default `
 Hi! We're BYCombo, a nonprofit dedicated to launching a revolution of musical masterpieces.
 
-Are you an unknown artist who's ready to craft a masterpiece? If you're willing to commit forty hours a week for as many years as it takes, then BYCombo is here for you. We'll put you in a band with others as ambitious as you are, provide you with guidance and a supportive community, and work with the gatekeepers to ensure your masterpiece is given fair consideration.
+Are you an unknown artist who's ready to craft a masterpiece? If you're willing to commit forty hours a week for as many years as it takes, then BYCombo is here for you.
 
-And BYCombo isn't just for musicians. We welcome anyone who hopes to challenge the accepted notion of who belongs in a band and what a band creates. Comic book artists, game developers, and all others, please don't hesitate to apply!
+We'll put you in a band with others as ambitious as you are, help you develop your big idea, provide you with a supportive community, and work with the gatekeepers to ensure your masterpiece is given fair consideration.
+
+And BYCombo isn't just for musicians. We welcome anyone who hopes to challenge the accepted notion of who belongs in a band and what a band creates. Comic book artists, game developers, and others, please don't hesitate to apply!
 `
