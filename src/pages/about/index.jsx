871df869ea6a05e8a611b3176d3117f083eef620
Bennett <bennettl@zillowgroup.com>
2022-05-07 02:28:36 -0700

    Yet another change.

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -7,13 +7,13 @@ const Component = () => (
             body: `
 # Why BYCombo?
 
-Hi, I'm Bennett from the Bobtail Yearlings. I've spent half my life arguing that indie rock has made music worse, to no avail. But something amazing has happened in recent years: Young people have started to agree with me. Which means the interdependence revolution can begin!
+Hi, I'm Bennett from the Bobtail Yearlings. I've spent half my life arguing that indie rock has made rock music worse, to no avail. But something amazing has happened in recent years: People have finally started agreeing with me. Which means the interdependence revolution can begin!
 
-There's just one problem: Indie rock isn't so much a genre as it is a system of rule. How can we revolt against a system, when systems prevent future revolution by design? Luckily, I've spent half my life figuring this out as well. BYCombo's solution is to assemble bands who are committed to making demos of historical consequence.
+There's just one problem: Indie rock isn't so much a genre as it is a system of rule. How can we revolt against a system, when systems prevent future revolution by design? Luckily, I've spent half my life figuring this out as well. The solution is for BYCombo to assemble bands who are committed to making demos of historical consequence.
 
 ### Interdependence 101
 
-Why historical consequence? Because when people say that music has gotten worse, this is really what they have in mind. Yes, good art is still being made, but history no longer cares by default. What's needed from today's artists is an explicit intent to last, and a massive effort to ensure it. And this will only happen through top-down initiatives like BYCombo.
+Why historical consequence? Because when people say that rock music has gotten worse, this is really what they have in mind. Yes, good art is still being made, but history no longer cares by default. What's needed from today's artists is an explicit intent to last, and a massive effort to ensure it. And this will only happen through top-down initiatives like BYCombo.
 
 Why demos? Because to make consequential art, a band needs to build exceptional strengths. But this isn't humanly possible unless they stay weak in areas that a label will later cover. Which is to say, consequential art can only happen interdependently. It needs to partner with a label, and demos are what a band sounds like until they've found one.
 
@@ -21,15 +21,15 @@ Why assembled bands? Because there are revolutionary bands still to come; they'r
 
 ### Serendipity favors a single hat
 
-Indie rock requires artists to wear multiple hats: the manager hat, the publicist hat… all of which take up precious time. Of course, the best indie artists don't skimp on wearing the artist hat, so their art is always as good as one should expect for their level of talent.
+Indie rock requires artists to wear multiple hats: the manager hat, the publicist hat… of course, the best indie artists don't skimp on wearing the artist hat. Their art is always as good as one should expect for their level of talent.
 
-But the best works of art are *better* than the artists who make them. Why? Because such works also benefit from countless moments of serendipity that can't be repeated at will. And when serendipity strikes, its benefits go to whichever hat you're wearing. So if you only care to wear the artist hat, then BYCombo is here for you. You want your art to be better than you, and so do we.
+But the best works of art are *better* than the artists who make them. Why? Because such works also benefit from countless moments of serendipity that can't be repeated at will. And when serendipity strikes, its benefits go to whichever hat you're wearing. So if you only care for the artist hat, then BYCombo is here for you. You want your art to be better than you, and so do we.
 
 ### Middling power leads to risk aversion
 
 In his book *Originals*, Adam Grant points out that in any hierarchy, the ones eager to take risks are those at the bottom with nothing to lose, and those safely perched at the top. By contrast, those in the middle are the most risk averse. This might explain why indie rock is so unoriginal. Its artists seek power while its labels relinquish it, so *no one* takes any risks.
 
-Because indie artists strive for power from day one, they're immediately beholden to the current paradigm that empowers them. They then fail to develop an instinct for questioning *any* paradigm. So at BYCombo, we encourage bands to treat their powerless years as a creative asset. What paradigm is your art free to challenge, while you have no power to safeguard?
+Because indie artists strive for power from the get-go, they're immediately beholden to the current paradigm that empowers them. And then they never learn to question *any* paradigm. This is why BYCombo encourages bands to treat their powerless years as a creative asset. What paradigms are you free to challenge, while you have no power to safeguard?
 
 ### The masses join the revolution last
 
@@ -37,19 +37,19 @@ Meanwhile, because indie labels *lack* power, they only make safe bets. And the 
 
 Revolutions that challenge the current paradigm may *start* with powerless outsiders, but they're *popularized* by powerful insiders at the top. Far from just exploiting such movements, these insiders play a crucial role in getting buy-in from the masses. This may not jibe with anyone's anti-establishment fantasy, but it's the historical truth. So BYCombo doesn't fight this truth; we work with it.
 
-### Responsible revolutions anticipate runaway effects
+### Responsible revolutions plan for runaway effects
 
-Indie rock gives artists the tools they need to bypass the middleman, and nothing more. But if the middleman's job is straightforward enough for artists to do themselves, then wealthy artists can just as easily pay *others* to do it. So indie rock devolved into a trust fund playground, leading to the disillusionment of today's young people. Yet this was the foreseeable endgame all along!
+Indie rock is a culture of self-reliance that gives artists the tools needed to bypass the middleman. But if the middleman's job is simple enough for artists to do themselves, then any wealthy artist can just as easily pay *others* to do it. As a result, indie rock has devolved into a trust fund playground, leading to today's disillusionment. Yet this was foreseeable from day one!
 
-Will BYCombo devolve as well? I doubt it. Our solution, taken to its absurd conclusion, calls for spending insane chunks of lifetime while combining brazenly diverse mediums. But if anything, the bands who do this will likely end up making the most consequential works of the century.
+Will BYCombo devolve as well? I doubt it. Our solution, taken to its absurd conclusion, calls for spending insane chunks of lifetime while combining brazenly diverse mediums. But if anything, the bands who do this will likely end up making the most consequential works of the century!
 
 ### For hungry outsiders, by a hungry outsider
 
-Who foresaw indie rock's absurd conclusion? Minorities did— which is why they've never started an indie movement. Instead, their movements reward what can't be bought: talent and originality born from hunger. And this is what BYCombo does.
+Who foresaw indie rock's absurd conclusion? Minorities did— which is why they've never founded a culture that values self-reliance. Instead, their movements reward what can't be bought: talent and originality born from hunger. And this is what BYCombo does.
 
-Yes, indie rock has recently embraced female and minority artists. But don't be fooled: They've been handed the reins *because* it's in decline. These are crumbs being offered here. Worse, history may remember this belated influx of diversity as the *cause* of indie rock's demise. Meanwhile, BYCombo helps women and minorities aim for the *top* of rock music's pantheon.
+Yes, indie rock has recently embraced female and minority artists. But don't be fooled: They've been handed the reins *because* it's in decline. These are crumbs being offered here. Worse, history may remember this belated influx of diversity as the *cause* of indie rock's demise. Meanwhile, BYCombo helps women and minorities aim for the *top* of rock and roll's pantheon.
 
-### Art needs martyrs
+### Art needs the blood of martyrs
 
 
     `,
