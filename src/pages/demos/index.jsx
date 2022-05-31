e444d52015c8b9b6afc941ecd6a61b756fdb99d3
Bennett Lin <bennettslin@gmail.com>
2022-05-31 00:10:04 -0700

    One more minor change.

--- src/pages/demos/index.jsx
+++ src/pages/demos/index.jsx
@@ -10,7 +10,7 @@ const Component = () => (
                 `
 # Return of the rock and roll martyrs
 
-Each band in our registry aimed to make rock history by putting 25,000 or more hours into a demo that advances the art form. Please contact the band directly if you have an offer or opportunity for them to consider!
+Each band in our registry aimed to make rock history by putting 25,000 or more hours into a demo that advances the art form. Please contact any band directly if you have an offer or opportunity for them to consider!
                 `,
                 (
                     <DemoLink
