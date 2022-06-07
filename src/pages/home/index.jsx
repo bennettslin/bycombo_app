26ac5043a025ca6617a0a987adef2693c3d8cf12
Bennett <bennettl@zillowgroup.com>
2022-06-07 00:37:29 -0700

    More content changes.

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -11,21 +11,19 @@ const Component = () => (
 
 ### Tools… are *rules*
 
-Indie rock loves to provide artists with tools of empowerment. These tools require the artist's time and commitment— and yet they offer no advantage over others, since *every* artist is expected to use them. Instead, they simply penalize the few who *don't*. But… this is how *rules* work! In other words:
+Indie rock loves to provide artists with tools of empowerment. These tools require the artist's time and commitment— yet they offer no advantage over others, since *every* artist is expected to use them. Instead, they just penalize the few who *don't*. But… this is how *rules* work! In other words:
 
 **Every tool of empowerment is just one more *rule* to follow.**
 
-// TODO: Finalize heading${'  '}
 ### Calling for a truce
 
-// TODO: Rephrase${'  '}
-Hi, I'm Bennett. Personally, I *hate* rules— yet if everyone felt the same, then breaking the rules would be the new rule! So I get why my attitude hurts me and me alone. But… I'm going to break them anyway. Rock history mainly remembers the rule breakers, and I *will* be remembered! So here's a truce:
+Hi, I'm Bennett. Personally, I *hate* rules— yet if everyone felt the same, then breaking the rules would be the new rule! So I get why my attitude sets me back. But… I'm going to break the rules anyway. Because rock history mainly remembers the rule breakers, and I want to be remembered! So how about this for a truce:
 
-**Since rules are made to be followed, rule breakers can't complain about facing a *natural* disadvantage. But since breaking the rules is also how history gets made, let's not impose *systemic* barriers on them as well.**
+**Since rules are made to be followed, rule breakers can't complain about facing *natural* disadvantages. But since breaking the rules is also how history gets made, let's not impose *systemic* barriers on them as well.**
 
 ### A thousand *Yearling's Bobtail*s
 
-So how did *I* choose to overcome my natural disadvantage? Given all the time I saved by *not* using indie rock's tools, I spent 25,000 hours— twelve full-time *years*!— to make the music and webcomic for *Yearling's Bobtail*.
+So how did *I* choose to overcome my natural disadvantages? Given all the time I saved by *not* using indie rock's tools, I spent 25,000 hours— twelve full-time *years*!— to make the music and webcomic for *Yearling's Bobtail*.
 
 Now, rule breakers like me aren't common— but we're also not rare! Picture a thousand other bands, each sacrificing a serious chunk of lifetime to advance the art form with a demo of their own. It would be the next revolution!
 
@@ -35,8 +33,10 @@ And yet… I can say from experience that indie rock won't notice such efforts. 
 
 **Once the gatekeepers fail us, there's no way for us to know. Which means they'll be failing us for a long time— if they haven't been *already*.**
 
-// TODO: Confirm wording${'  '}
-In other words, indie rock has violated our truce, and now these systemic barriers are insurmountable. To be fair, there's been a role reversal: Breaking the rules now requires far *more* effort, not *less*. So maybe the gatekeepers don't realize that they've shut out the rule breakers, because they still expect them to come from the opposite extreme!
+// TODO: Reconsider whether to have this paragraph at all${'  '}
+So indie rock has violated our truce, and now these systemic barriers are insurmountable. To be fair, maybe the gatekeepers don't realize that they're shutting out the rule breakers. Breaking the rules now requires *more* effort, not *less*, and this role reversal is fairly recent.
+
+because they still expect them to come from the opposite extreme! there *has* been a role reversal: . 
 
 // TODO: Finalize heading${'  '}
 ### Uniting the rock gadflies
@@ -49,27 +49,6 @@ who aren't content to accept whatever the gatekeepers tell them, and are pretty 
 But with BYCombo, the public can now discover any bands breaking the rules to make history, who risk being ignored by the indie gatekeepers!
 
 gadfly
-
-// TODO: Move to Apply page${'  '}
-### Who we're looking for
-
-BYCombo is open to any artist eager to sacrifice a serious chunk of lifetime for the interdependence revolution! You're also one of the following:
-
-* A rock musician— singer, guitarist, drummer…
-* A musician of another stripe— cellist, tabla player…
-* An artist of some other medium— dancer, web developer…
-* A creator with a bold vision for a musical project
-
-Once we put you in a band with three or four others— subject to everyone's approval— you'll all relocate to Los Angeles or another major city. Each of you will then put in enough time on your band project for the sum total to be 25,000 hours.
-
-### What we'll do for you
-
-Through the years, BYCombo will advise you on your band project. If the project requires unique collaborators— acrobats? puppeteers?— we'll help you find them. We'll also include you in a supportive community of other ambitious bands. And finally, the Bobtail Yearlings will help when they can!
-
-// TODO: Include writers and gadflies${'  '}
-Once your demo is complete, we'll send it to any record labels or industry insiders who sign up to hear it first. In exchange for this privilege, they'll promise to give it fair consideration and a timely response.
-
-Finally, we'll add your demo to our public registry. With your help, BYCombo will become the public's trusted resource to hear about all the latest bands sacrificing serious chunks of lifetime to make history!
             `,
         }}
     />
