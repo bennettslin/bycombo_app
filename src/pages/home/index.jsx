31906bde24078fb154abdcc21f0a131537a3a227
Bennett Lin <bennettslin@gmail.com>
2022-06-21 02:06:35 -0700

    More content changes.

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -9,11 +9,11 @@ const Component = () => (
 
 BYCombo is a nonprofit incubator of rock bands.
 
-**Our mission is to launch the interdependence revolution, and to support our bands who'll make interdependence the most consequential rock movement of the 21st century!**
+**Our mission is to launch the interdependence revolution, and to support the bands who'll make interdependence the most consequential rock movement of the 21st century!**
 
 ### Tools… are *rules*
 
-Indie rock seeks to lower the barriers to entry by providing artists with tools of empowerment. These tools aren't actually free: They cost the artist's time and energy. And yet they offer no advantage in the end, since *every* artist is expected to use them. They just penalize the ones who *don't*. But… this is how *rules* work! In other words:
+Indie rock seeks to lower the barriers to entry by providing artists with tools of empowerment. These tools aren't free, though: They cost the artist's time and energy. And yet they offer no advantage in the end, since *every* artist is expected to use them. So they just penalize the ones who *don't*. But… this is how *rules* work! In other words:
 
 **Every tool of empowerment becomes just another *rule* to follow.**
 
@@ -25,11 +25,11 @@ Hi, I'm Bennett. I think indie rock's rules hurt the art form, and I refuse to f
 
 ### A thousand *Yearling's Bobtail*s
 
-So a rule breaker's biggest disadvantage is this: Because they aren't thrilled to put *any* time or energy into tools of empowerment, others treat them as lazy or inept. To counter this misreading, their only option is to pour *all* their time and energy into advancing the art form, with an ambitious demo that *couldn't* have been made for any less effort. Yet think of what this means:
+The biggest disadvantage is this: Because rule breakers aren't thrilled to put *any* time or energy into tools of empowerment, others treat them as lazy or inept. To counter this misreading, their only option is to pour *all* their time and energy instead into advancing the art form, with an ambitious demo that *couldn't* have been made for any less effort. Yet think of what this means:
 
 **Rule breakers won't just be making the *disruptive* works of our time— their works will be our generation's most *accomplished* as well.**
 
-Which is why I spent 25,000 hours— twelve full-time *years*!— to make the music and webcomic for *Yearling's Bobtail*. And yes, doublespeaker rhyme *couldn't* have come to me any other way. Of course, I'm not the only one who'd rather double down on making art. Picture a thousand bands, each making an ambitious demo of their own. It would be the next revolution!
+Which is why I spent 25,000 hours— twelve full-time *years*!— to make the music and webcomic for *Yearling's Bobtail*. And yes, doublespeaker rhyme *couldn't* have come about any other way. Of course, I'm not the only one who'd rather double down on making art. Picture a thousand bands, each making an ambitious demo of their own. It would be the next revolution!
 
 ### Who gatekeeps the gatekeepers?
 
@@ -39,11 +39,9 @@ And yet… indie rock isn't set up to care. When barriers to entry are lowered, 
 
 ### An anti-indie counterculture
 
+// TODO: All of this section${'  '}
 The situation might seem hopeless. Luckily, some music fans *also* find indie rock's rules absurd, and would rather break them as well! They know that lowering barriers to entry only raises them elsewhere. And they want to be *awestruck* by artists, not to *support* them. Today's rock just doesn't seem destined to make history, which tells them that something is very wrong.
 
-Intuitively recognize that today's rule breakers are making ambitious demos.
-
-// TODO: Socratic gadfly${'  '}
 These disgruntled music fans are rock's last hope. They already have the *will* to challenge the indie gatekeepers. Now with BYCombo, they finally have the *way*.
             `,
         }}
