d14f92e35357ca029bddab64c1f52d48df69570c
Bennett Lin <bennettslin@gmail.com>
2023-04-05 02:55:21 -0700

    Revise curators section.

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -47,21 +47,13 @@ So rock is waiting for… a self-sacrificing martyr. But martyrs are the ones wh
 
 Well… not so fast. As barriers to entry are lowered, the applicant pool gets flooded, overwhelming the indie labels and other curators. This makes them insist all the more firmly that artists use indie rock's tools, which serve to keep every work bite-sized and uniform with others.
 
-${'  ' || `
-// TODO: Connect sentences better?
-`}
-
-It's worse than just that, however. Because these indie curators stay close to their local scenes, they're seen as the "good guys." What they like is simply what the people like, and so we trust their judgment. But… studies show that it's human nature to [*dislike* creativity](https://www.nytimes.com/2022/04/16/science/creativity-implicit-bias.html)!
+It's worse than just that, however. We see these indie curators as the "good guys" because they stay close to the underground scenes. This makes them one of us, so whatever they like, we trust that we'll like. But… according to studies, it's human nature to [*dislike* creativity](https://www.nytimes.com/2022/04/16/science/creativity-implicit-bias.html)!
 
 Creative ideas are unfamiliar, and this makes us uncomfortable. Now, the corporate "bad guys" just ignore this discomfort, since it's *all* unfamiliar to them. But the good guys pride themselves on their hard-won instincts, so they interpret the discomfort as a sincere warning from their gut. Ironically, then, as [Frank Zappa noted](https://youtu.be/xP4wsURn3rw), this makes them *worse* at discovering creativity!
 
-${'  ' || `
-// TODO: Just review these two paragraphs now.
-`}
+But worst of all, any band who breaks the good guys' rules is technically a bad guy as well— and indie rock came here to shut out *all* the bad guys! Which brings us to the heart of the problem:
 
-But worst of all, any band who breaks the good guys' rules is technically a bad guy as well— and indie rock is here to shut out *all* the bad guys! Which brings us to the heart of the problem:
-
-**Indie rock didn't just lock its rules in place, believing in the universality of its ideals; it's the *first* movement in rock to do so. So there's no historical precedent to teach us how to dissent— or even to assure us that we *can*.**
+**Indie rock didn't just lock its rules in place, believing in the universality of its ideals; it's the *first* rock movement *ever* to do so. So there's no historical precedent to teach us how to dissent— or even to assure us that we *can*.**
 
 ### A hundred *Yearling's Bobtail*s
 
