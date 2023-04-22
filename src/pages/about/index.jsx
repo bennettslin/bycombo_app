da720c5e10adc70c947b684ee209335b1bba2714
Bennett Lin <bennettslin@gmail.com>
2023-04-21 20:50:48 -0700

    Completely change outsiders section.

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -115,11 +115,17 @@ There's nothing wrong with indie rock's artistic libertarianism— but it's a *p
 
 ### *For* hungry outsiders, *by* a hungry outsider
 
+${'  ' || `
+// TODO: Just review a couple more times.
+`}
+
 Who foresaw indie rock's final stage as a trust fund playground? We did— us women and minorities— which is why *we've* never founded a movement to compete on self-reliance. By the way, don't be misled by hip hop's bravado: When rappers boast of having hustled, they're saying, "Hey, I hustled *too*." It's to *prevent* self-reliance alone from having any worth.
 
 Yet despite the poor fit, indie rock sure is eager of late to hand over its reins to us. Why? Because it's in *decline*! With little of historical consequence to show after two decades, indie rock has been demoted to just another foreign canon. So we women and minorities are being set up to [take the fall](https://www.investopedia.com/terms/g/glass-cliff.asp)…
 
-No thanks! The best movements *for* us are the ones founded *by* us. They offer what *we* want— *mutual* reliance, and a chance at the *historical* canon— in exchange for what *can't* be bought: originality and talent, born from an outsider's hunger. And if hip hop was the last case in point, then BYCombo will be the next.
+No thanks! Never forget: Rock and roll was founded by Blacks and queers, then carried along by rural Southerners, Midwestern Jews, working-class Brits… In short, it's a history of hungry outsiders. Which means aiming for the historical canon should be the natural instinct of today's female and minority artists— and *would* be, had indie rock not intervened.
+
+To be loved at all, outsiders need to be the *most* original, the *most* talented… which is humanly possible only through *mutual* reliance. This is why hip hop, not indie rock, is rock and roll's true heir! But now with BYCombo, you can once again aim for the historical canon— as a rock band.
 
 ${'  ' || `
 // TODO: ### Independence breeds homogeneity
