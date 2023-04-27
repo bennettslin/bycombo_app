9d186d31e3daeca152f2132de037df1a9ce85ac6
Bennett Lin <bennettslin@gmail.com>
2023-04-26 18:50:02 -0700

    Revise creativity section.

--- src/pages/indie/index.jsx
+++ src/pages/indie/index.jsx
@@ -91,7 +91,11 @@ Yet no one cares what past critics thought, while nearly everyone loves a past w
 
 ### Creativity gets its chance from the *clueless*
 
-Because the indie labels stay close to the local scenes, they're both "in the know" and "one of us." So whatever *they* like is the best of what *we'll* like, right? Yet according to studies… it's human nature to [*dis*-like creativity](https://www.nytimes.com/2022/04/16/science/creativity-implicit-bias.html)!
+${'  ' || `
+// TODO: Just one more final review.
+`}
+
+Because the indie labels stay close to the local scenes, they're both "in the know" and "one of us." So what *they* like is whatever those like us have liked, which means it's the best of what *we'll* like, right? Yet according to studies… it's human nature to [*dis*-like creativity](https://www.nytimes.com/2022/04/16/science/creativity-implicit-bias.html)!
 
 Creative ideas are unfamiliar, and this makes us uncomfortable. Now, the corporate suits just ignore this discomfort, since it's *all* unfamiliar to them. But those in the know pride themselves on their hard-won instincts, so they interpret the discomfort as a sincere warning from their gut. Ironically, then, as [Frank Zappa noted](https://youtu.be/xP4wsURn3rw), this makes them *worse* at discovering creativity!
 
