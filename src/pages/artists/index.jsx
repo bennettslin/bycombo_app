7a1114c7e6699745e7f0eb1139a4ffdd341631f8
Bennett Lin <bennettslin@gmail.com>
2023-05-08 03:21:38 -0700

    Audit artists page.

--- src/pages/artists/index.jsx
+++ src/pages/artists/index.jsx
@@ -9,7 +9,7 @@ const Component = () => (
 # Join the interdependence revolution!
 
 ${'  ' || `
-// TODO: Just a few more reviews.
+// TODO: Just one more final review.
 `}
 
 As an artist, do you sense that everything about rock today is *backward*? Why do the indie gatekeepers force you to waste so much time and energy using their tools? This *can't* be what the public wants, nor what matters to history… Well, you're *not* crazy: The system *is* broken— and here's how we'll fix it!
@@ -18,7 +18,7 @@ As an artist, do you sense that everything about rock today is *backward*? Why d
 
 ### Who we're looking for
 
-Anyone of legal age is welcome to apply. But ideally, you should have some life experience under your belt! You're also reasonably skilled in one or more of the following roles:
+Anyone of legal age is welcome to apply. But ideally, you should already have some life experience under your belt! You're also reasonably skilled in one or more of the following roles:
 
 * A rock musician: singer, guitarist, drummer…
 * A musician of any other stripe: composer, tabla player…
@@ -30,7 +30,7 @@ As a rock movement *started* by a minority, BYCombo was *conceived* to give you 
 
 ### How you'll form a band
 
-We'll set you up with prospective bandmates who match your artistic tastes and skill levels. The ideal is to have gender diversity in every band.
+If we accept you, we'll set you up with prospective bandmates who match your artistic tastes and skill levels. Each band will be balanced in gender to the furthest degree possible, and no band will be single gender.
 
 Upon reviewing each other's links and videos, you'll meet online. If everyone agrees to form a band, you'll all relocate to Los Angeles, or another major city. After you've settled in, each of you will put in enough time on your band project for the sum total to be at least 25,000 hours.
 
@@ -42,7 +42,7 @@ ${'  ' || `
 
 BYCombo will provide a forum for you and the others in your cohort to support each other, and we'll periodically advise you on your band project. We also encourage every alum to pay it forward and collaborate with our bands in the next cohort!
 
-Once your demo is complete, we'll add it to our public registry and notify everyone on our mailing list. Over the years, each new BYCombo band will raise awareness of our mission, boosting your own chances of getting heard. Of course, we're just getting started, so to thank you for your patience during this initial stage:
+Once your demo is complete, we'll add it to our public registry and notify everyone on our mailing list. Over time, each new BYCombo band will raise awareness of our mission, boosting your chances to get heard. Of course, we're just getting started, so to thank you for your patience during these early years:
 
 **The Bobtail Yearlings will invite up to ten bands from our pilot cohort to feature as guest artists on their next project— an album about the life of English biochemist Rosalind Franklin!**
 
@@ -63,7 +63,7 @@ Email us the following information, and we'll be in touch:
 
 If you're co-applying with one or more bandmates, please send a single email with each person's information. If you have special circumstances that need to be considered, please let us know. We'll see if we can make things work.
 
-And remember, as a nonprofit, BYCombo will never charge a fee or take a cut of your earnings. If you're willing to self-sacrifice for art's sake, then we'll do all we can to serve you!
+And remember, as a nonprofit, BYCombo will never charge a fee or take a cut of your earnings. If you're willing to self-sacrifice for art's sake, then we're here to serve you!
             `,
         }}
     />
--- src/pages/registry/index.jsx
+++ src/pages/registry/index.jsx
@@ -12,7 +12,7 @@ const Component = () => (
 
 **To earn the listener's trust, each of our bands sacrificed a serious chunk of lifetime— at least 25,000 hours, or twelve full-time years— to make a demo that advances the art form.**
 
-But remember, our bands are required to *ignore* indie rock's tools, which means the indie gatekeepers will *refuse* to hear these demos. So if rock is to be saved, we need *others* to tell the world about our bands. Can one of these rock saviors… be you?
+But remember, our bands are required to *ignore* indie rock's tools— which means the indie gatekeepers will *refuse* to hear these demos. So if rock is to be saved, we need *others* to tell the world about our bands. Can one of these rock saviors… be you?
 
 If you have an offer or opportunity for any of our bands to consider, please contact them directly!
                 `,
