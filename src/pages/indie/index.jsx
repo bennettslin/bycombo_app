104f38312ee604ac871e7252e99e73b5712bd871
Bennett Lin <bennettslin@gmail.com>
2023-05-13 17:14:36 -0700

    Final audit of creativity section.

--- src/pages/indie/index.jsx
+++ src/pages/indie/index.jsx
@@ -95,10 +95,6 @@ Yet no one cares what a past critic thought, while nearly everyone loves a past 
 
 ### Creativity gets its chance from the clueless
 
-${'  ' || `
-// TODO: Just one more final review.
-`}
-
 Because the indie gatekeepers stay close to their local scenes, they're both "in the know" and "one of us." So whatever *they* like is what others like *us* have liked, which means it's the best of what *we'll* like… right? Yet according to science, it's human nature to [*dis*-like creativity](https://www.nytimes.com/2022/04/16/science/creativity-implicit-bias.html)!
 
 Creative ideas are unfamiliar, and this makes us uncomfortable. Now, the corporate suits just ignore this discomfort, since it's *all* unfamiliar to them. But those in the know pride themselves on their hard-won instincts, so they interpret the discomfort as a sincere warning from their gut. Ironically, then, as [Frank Zappa noted](https://youtu.be/xP4wsURn3rw), this makes them *worse* at discovering creativity!
