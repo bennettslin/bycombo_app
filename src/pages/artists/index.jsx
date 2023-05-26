1b3832430643513345683e77ca34abda325ad52d
Bennett Lin <bennettslin@gmail.com>
2023-05-25 18:02:51 -0700

    Final audit of gatekeepers section.

--- src/pages/artists/index.jsx
+++ src/pages/artists/index.jsx
@@ -14,10 +14,6 @@ As an artist, do you sense that everything about rock today is *backward*? Why d
 
 ### Who we're looking for
 
-${'  ' || `
-// TODO: Must be driven.
-`}
-
 Anyone of legal age is welcome to apply. But ideally, you should already have some life experience under your belt! You're also reasonably skilled in one or more of the following roles:
 
 * A rock musician: singer, guitarist, drummer…
