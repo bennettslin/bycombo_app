f6e48bc3933d5d850076e78fda5509f7543a958b
Bennett Lin <bennettslin@gmail.com>
2023-08-08 18:31:53 -0700

    Begin work on middle managers section.

--- src/pages/indie/index.jsx
+++ src/pages/indie/index.jsx
@@ -118,30 +118,15 @@ Which is why, throughout history, the *most* creative artists were backed by tho
 
 ### Middle managers join the revolution last
 
+${'  ' || `
+// TODO: A full revamp isn't needed. But tweak in light of recent additions. Change predictive analytics. Maybe say fearless entrepreneurs? Make this last sentence really good.
+`}
+
 In his book [*Originals*](https://adamgrant.net/book/originals), Adam Grant points out that in any business hierarchy, the ones most eager to take risks are those safely perched at the top, as well as those at the bottom with nothing to lose. Those in the middle, by contrast, are the most risk-averse. And who are these risk-averse middle managers in *rock*? The indie labels.
 
 Because indie labels have just enough standing to lose, they only make *safe* bets. And the safest bet is to deliver what the people already want. But this creates an echo chamber— the same kind that makes predictive analytics so detrimental to art. Even worse, every revolution gets nipped in the bud, because the [people *never* ask](https://www.goodreads.com/quotes/988332) for what they can't envision on their own.
 
-Revolutions may *start* with artists, but they're *spread* by business partners, whose know-how is needed to win over the public. This makes it crucial for such artists to find partners who share their appetite for risk. So BYCombo helps you reach industry executives who are well-funded enough to make *wild* bets, as well as budding entrepreneurs with no standing to lose.
-
-${'  ' || `
-// TODO: ### Moderate standing leads to risk aversion
-
-### Moderate standing leads to risk aversion
-
-In the long term, maybe include how both artists *and* gatekeepers today are risk averse? Make this more about how the optimal situation for risk taking is that artists are initially powerless, and gatekeepers are powerful, rather than both being moderately empowered.
-
-The problem is that this would then be similar to paid dues and passion of martyrs. But maybe this can be fixed by moving it closer down, making their connection intentional.
-
-In his book [*Originals*](https://adamgrant.net/book/originals), Adam Grant points out that in any business hierarchy, the ones most eager to take risks are those safely perched at the top, as well as those at the bottom with nothing to lose. Those in the middle, by contrast, are the most risk-averse— because they have just enough standing to lose.
-
-The maximal arrangement for taking risks, then, is the one we had in the past, where powerless artists 
-
-But you know where this is going: Indie rock hated this arrangement.
-Meanwhile, the worst situation for taking risks is the one we have now.
-
-Make the point that as a nonprofit, BYCombo also has nothing to lose.
-`}
+Revolutions may *start* with artists, but they're *spread* by business partners, whose know-how is needed to win over the public. This makes it crucial for such artists to find partners who share their appetite for risk. So BYCombo helps you reach industry executives who are well-funded enough to make *smart* bets, as well as budding entrepreneurs with no standing to lose.
 
 ### The grassroots is now a lottery
 
