d39b201e2d905c2e26fdb6e547282e7388417ecb
Bennett Lin <bennettslin@gmail.com>
2023-08-15 14:05:07 -0700

    Revise peer review section.

--- src/pages/indie/index.jsx
+++ src/pages/indie/index.jsx
@@ -160,7 +160,7 @@ Here's the thing: Most artists in the historical canon were superstars in their 
 
 Um… all of us other artists who *weren't* picked, that's who! To be clear, we're not [crabs in a bucket](https://en.wikipedia.org/wiki/Crab_mentality). But if a critical darling strikes us as a lottery winner, we won't pretend otherwise. And this gets at why critics are so bad at making timeless judgments: They don't consider how an artist looks to *other* artists— especially those of us who remain unknown.
 
-Because we're invested in the creative process ourselves, we commit fewer false positives: We know when talents once considered notable are now par for the course. And we commit fewer false *negatives*: We'll give crazy new ideas a chance, knowing the courage it takes to propose them. In short, what historians will see in *time*… other artists come closest to seeing *now*.
+Because we're invested in the creative process ourselves, we commit fewer false positives: We know when talents once deemed notable are now par for the course. And we commit fewer false *negatives*: We'll give crazy new ideas a chance, knowing the courage it takes to carry them through. In short, what historians will see in *time*… other artists come closest to seeing *today*.
 
 So ignore the critics! To make history without first achieving stardom, your talents must inspire the envy of your fellow *artists*. Which is why BYCombo includes you in a community of peers who'll keep your standards high.
 
