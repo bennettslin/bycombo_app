fffe7d65a6bae6d4f971ae547c0c081e3c77cc2f
Bennett Lin <bennettslin@gmail.com>
2023-09-12 00:44:56 -0700

    Update links.

--- src/content/registry/2023/bobtail-yearlings/yearlings-bobtail.js
+++ src/content/registry/2023/bobtail-yearlings/yearlings-bobtail.js
@@ -35,7 +35,7 @@ After attending a coding bootcamp, Bennett worked for years as a coder while liv
 
 **Who are we looking for?**
 
-We're looking for someone to join our band in a full-time [admin position](https://www.bobtailyearlings.com/essays/ongoing/our-mission-principles-and-terms/#roles), as an [equally paid](https://www.bobtailyearlings.com/essays/ongoing/our-mission-principles-and-terms/#compensation) member. You would handle our administrative work, take on a leadership role at BYCombo, and oversee development of Bobtail Method and Bobtail Dominoes.
+We're looking for someone to join our band in a full-time [admin position](https://www.bobtailyearlings.com/writings/ongoing/our-mission-principles-and-terms/#roles), as an [equally paid](https://www.bobtailyearlings.com/writings/ongoing/our-mission-principles-and-terms/#compensation) member. You would handle our administrative work, take on a leadership role at BYCombo, and oversee development of Bobtail Method and Bobtail Dominoes.
 
 We also hope to hear from business partners who'd like to work with our band in any way: managers, publicists, and labels.
     `,
