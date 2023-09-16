be3b3399674544ddb02ad5724f7178dd3b4faf8e
Bennett Lin <bennettslin@gmail.com>
2023-09-16 10:44:03 -0700

    Get rid of some social media constants.

--- src/constants/socialMedia.js
+++ src/constants/socialMedia.js
@@ -1,6 +1,2 @@
 export const COPY_URL_KEY = 'copyUrl'
-export const FACEBOOK_KEY = 'facebook'
-export const TWITTER_KEY = 'twitter'
-
-export const FACEBOOK_ACCOUNT = 'BeWhyCombo'
 export const TWITTER_ACCOUNT = 'BeWhyCombo'
