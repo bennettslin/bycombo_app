5a6bae6fbe1ef1a523a6c10ea2c7827f98092a57
Bennett Lin <bennettslin@gmail.com>
2023-11-03 02:10:41 -0700

    Add todo to registry page.

--- src/pages/registry/index.jsx
+++ src/pages/registry/index.jsx
@@ -15,9 +15,13 @@ const Component = () => (
                 `
 # ${TITLE}
 
+${'  ' || `
+// TODO: Mention instead tools meant to empower them.
+`}
+
 **To earn the trust of business partners and listeners alike, each band in our public registry sacrificed a serious chunk of lifetime— 25,000 hours, or twelve full-time years— to make a demo that advances the art form.**
 
-But remember, we require our bands *not* to care about fair compensation or self-empowerment— which means the indie gatekeepers will dismiss them as hobbyists! So if rock is to be saved, we need *others* to tell the world about them. Can one of these rock saviors… be you?
+But remember, we require our bands *not* to care about self-empowerment— which means the indie gatekeepers will dismiss them as hobbyists! So if rock is to be saved, we need *others* to tell the world about them. Can one of these rock saviors… be you?
 
 If you have an offer or opportunity for any of our bands to consider, please contact them directly!
                 `,
