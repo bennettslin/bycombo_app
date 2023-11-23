140fdba15a1cf14868080400f03acdd62e87254d
Bennett Lin <bennettslin@gmail.com>
2023-11-23 10:23:27 -0800

    Review some sections.

--- src/pages/indie/index.jsx
+++ src/pages/indie/index.jsx
@@ -136,11 +136,11 @@ ${'  ' || `
 // TODO: Review a few more times.
 `}
 
-We all respect those [80s indie bands](https://www.littlebrown.com/titles/michael-azerrad/our-band-could-be-your-life/9780316247184) who started from the grassroots, don't we? Because they were chosen by the people, *not* by the elite. Yet there's a less spoken reason as well: It feels like their success was *ensured*. That is to say, they earned it by showing up to work each day for untold years— unlike a one-hit wonder, who simply lucked out and won the virtual lottery.
+We all respect those [80s indie bands](https://www.littlebrown.com/titles/michael-azerrad/our-band-could-be-your-life/9780316247184) who started from the grassroots, don't we? After all, they were chosen by the people, *not* by the elite. Yet there's a less spoken reason as well: It feels like their success was *ensured*. That is to say, they earned it by showing up to work each day for untold years— unlike a one-hit wonder, who simply lucked out and won the virtual lottery.
 
 In those days, lottery players would sign lopsided deals with a major label, all for a single chance at exposure. But the Internet has since empowered them to reach the public directly, giving them unlimited chances— while making them indie artists in good faith! So now they're flooding the world with their music— *and* getting respect for it.
 
-Yet according to the indie gatekeepers, all this is as it should be! After all, their main concern is to see artists chosen by the people, not by the elite— and what are [machine learning](https://en.wikipedia.org/wiki/Machine_learning) algorithms, if not the new word of mouth? The grassroots, in essence, is still very much a [*populist*](https://en.wikipedia.org/wiki/Populism) domain.
+Yet according to the indie gatekeepers, all this is as it should be! Because ultimately, their main concern is for artists to be chosen by the people, not by the elite— and what are machine learning [algorithms](https://en.wikipedia.org/wiki/Algorithmic_bias), if not the new word of mouth? The grassroots, in essence, is still very much a [*populist*](https://en.wikipedia.org/wiki/Populism) domain.
 
 It's just that now, little but chance separates the few who make it from the many who don't, because the grassroots is no longer a [*meritocratic*](https://en.wikipedia.org/wiki/Meritocracy) domain. But… what if *you* were hoping to ensure your success? Then keep working on your craft for untold years— and let BYCombo expose you to a new elite!
 
@@ -228,8 +228,6 @@ ${'  ' || `
 // TODO: Change last paragraph to say that the real civil war in music today isn't between rockists and poptimists, but between rock and rollists and indie poptimists?
 
 Make no mistake: The real civil war in music today *isn't* between rock and pop. It's between outsider movements— "rock and rollists," perhaps?— and an underdog monoculture— let's say, the "indie poptimists." So BYCombo is here to ensure that hungry outsiders— the ones most likely to make history!— have a fighting chance.
-
-Review one more time.
 `}
 
 Perhaps you think these arguments against indie rock are moot. After all, didn't [poptimism defeat rockism](https://en.wikipedia.org/wiki/Rockism_and_poptimism) a decade ago? Well, no. It was the *indie* camp that killed rock as a genre. Then, believing the death to be natural, many in this camp switched to pop— and brought their good intentions with them. So poptimism… is just indie rock for the pop genre!
