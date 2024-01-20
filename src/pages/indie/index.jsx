5650dcd2021b75595c1d24e1ead17a5ee0e34dbf
Bennett Lin <bennettslin@gmail.com>
2024-01-19 16:34:21 -0800

    Yet more revisions.

--- src/pages/indie/index.jsx
+++ src/pages/indie/index.jsx
@@ -77,11 +77,11 @@ ${'  ' || `
 Also figure out best location for this section.
 `}
 
-Obviously, cutting out the middleman doesn't eliminate our need for the work they do. So what indie rock has *really* done is create a new hybrid role in which one person does the job of both artist *and* middleman. Now obviously, artists are well qualified for this role, since they're already skilled at *one* of these jobs. Yet… the same is true of middlemen!
+Obviously, cutting out the middleman doesn't eliminate our need for the work they do. So what indie rock has *really* done is create a new hybrid role in which one person does the job of both artist *and* middleman. Now, artists are obvious candidates for this role, since they're already skilled at *one* of these jobs. Yet… the same can be said of middlemen!
 
 And these two jobs *aren't* equal. Whereas the artist is loved for doing work that all of us enjoy, the middleman is hated for doing work that many of us dread. So in this new hybrid role, artists *have* to be their own middlemen— but middlemen *get* to be their own artists. Which tells us something: A world without middlemen… is a middleman's dream, *not* an artist's!
 
-In other words, the artists who most love to be indie are those who would otherwise be *middlemen*. Yet Millennials never question this world, since it's all they've ever known. Luckily, I came of age *before* indie rock took over, and so I've *always* dreamt of only doing the artist's job— as it's the only way to be the best at it! Now with BYCombo, you can have this dream as well.
+In other words, the artists who most love to be indie are those who would otherwise be *middlemen*. Yet Millennials never question this world, since it's all they've ever known. Luckily, I came of age *before* indie rock took over, and so I've *always* dreamt of only doing the artist's job— as there's no other way to be the best at it! Now with BYCombo, you can have this dream as well.
 
 ### ${++sectionCounter}. Cultures of grievance wallow in confusion
 
