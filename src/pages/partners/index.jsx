d1ec2696b81f9938b7f0084930517a064d24599f
Bennett Lin <bennettslin@gmail.com>
2024-02-02 22:45:46 -0800

    Keep making progress on partners page.

--- src/pages/partners/index.jsx
+++ src/pages/partners/index.jsx
@@ -17,26 +17,18 @@ const Component = () => (
 
 ${'  ' || `
 // TODO: Segue to bold text.
-
-The Beatles relied on managers from day one; it's how they got their Hamburg gig and their Parlophone audition. So if history's greatest rock band was a paragon of *inter*-dependence… then how good can any *indie*-pendent artist really be?
-
-Every BYCombo band promises to be an ideal partner in interdependence, by sacrificing a serious chunk of lifetime to advance the art form, while remaining powerless 
 `}
 
-As an entrepreneur, would you happily partner with today's artists, if only indie rock didn't have it so *backward*? Well, there's good news! The artists trying to make the best art today [*don't* wish](/indie#disintermediation-is-a-middlemans-dream) to be indie; they *do* hope to work with others who can handle their business matters. They just haven't had a way to make themselves known to you… until now!
+As an entrepreneur, would you readily partner with today's artists, if only indie rock didn't have it so *backward*? The Beatles relied on managers from day one; it's how they got their [Hamburg gig](https://en.wikipedia.org/wiki/Allan_Williams) and their [Parlophone audition](https://en.wikipedia.org/wiki/Brian_Epstein). And if *inter*-dependence is what allowed them to become history's greatest rock band… then how good can any *indie*-pendent artist really be?
 
-With BYCombo, you can once again discover the next generation of bands to make history. Here's how:
+Well, there's good news! The artists trying to make the best art today *don't* want to be indie; they *do* want partners to handle their business matters. There just hasn't been a way for you to hear about them… until now. Because, thanks to BYCombo, you can work with history-making bands once again! Here's how:
 
-**To partner with a BYCombo band allows entrepreneurs to partner with interdependent artists who promise to spend *all* their time and energy advancing the art form, in exchange for**
+**Every BYCombo band promises to be an ideal business partner by having sacrificed serious chunks of lifetime to advance the art form, even while remaining powerless to get heard on their own.**
 
-State that they can contact artist directly, and there are no fees.
+(Mention newsletter, or just visiting registry directly. State that they can contact artist directly, and there are no fees.)
 
 ${'  ' || `
-// TODO: Connect this all to the idea that the reader is an entrepreneur.
-
-Scraps:
-As an entrepreneur, have you grown tired of indie rock's low barriers to entry? BYCombo's bands will earn your trust by paying a steep *cost* of entry:
-
+// TODO:
 If you'd like to be notified when a new demo is added to our public registry, please subscribe to our newsletter!
 `}
 
@@ -50,22 +42,21 @@ If you'd like to be notified when a new demo is added to our public registry, pl
 ${'  ' || `
 // TODO: Keep working on this.
 
-First paragraph, second sentence, is a little unclear?
-
-As an entrepreneur interested in the arts,
-
 Make clear that it's a proposal.
 
-I'm trying to attract influencers, so make it less about business acumen and more about… whatever they're proud of.
+I'm trying to attract influencers, so make it less about business acumen and more about… whatever they're proud of. People skills?
 
-Read the Brian Epstein book!
-`}
-
+Scraps:
 If indie artists are never the best, then why are there *any*? Here's a theory: Perhaps the ones who most love to be their own manager… are those who, in the past, would have been resigned to managing *others*. So indie rock *doesn't* help the next Beatles retain their earnings; it lets today's Brian Epsteins enjoy respect— and the spotlight— as artists *themselves*!
 
-As an entrepreneur interested in the arts, maybe you've thought to take this path yourself. After all, what else is there to do, when indie rock only sees *artists* as the heroes, while all others who share in the earnings are *villains*? Why, you can join a counterculture of interdependence, and be *respected* for your business acumen— as a band manager!
+Read the Brian Epstein book!
 
-But… will you also get to enjoy the spotlight? Yes! With BYCombo, you can be a full-fledged member of the band. As such, you'll be given equal credit and royalties. And while there's no expectation for you to contribute artistically, any time you spend doing so will count toward your band's 25,000 hours.
+But… will you also get to enjoy the spotlight? Yes! With BYCombo, you can be a full-fledged member of the band. 
+`}
+
+Perhaps a moment in the spotlight yourself would be nice— but indie rock only sees artists as the heroes, while all others who share in the earnings are villains. So what's an entrepreneur like you to do? Become an [artist yourself](/indie#disintermediation-is-a-middlemans-dream)? Why, you can be respected for your business acumen and people skills— by being a band manager who's also a full-fledged member of the band!
+
+(Explain that this is an experimental proposal). As such, you'll be given equal credit and royalties. And while there's no expectation for you to contribute artistically, any time you spend doing so will count toward your band's 25,000 hours.
 
 ${'  ' || `
 // TODO: Describe plan.
