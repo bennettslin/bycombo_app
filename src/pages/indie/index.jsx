82c2f3c26d4731adbdb4a921ebbe0455a6190bca
Bennett Lin <bennettslin@gmail.com>
2024-02-24 02:22:46 -0800

    Last changes for the night.

--- src/pages/indie/index.jsx
+++ src/pages/indie/index.jsx
@@ -217,7 +217,7 @@ But this misses the point, doesn't it? Once "verification" is trivial to attain,
 
 Yet there's hope: While Millennials were active creators from day one, Gen Z reverted into passive consumers once the Internet consolidated. So as with Boomers and Gen X, Gen Z's artists relate foremost to the *audience*— having first been an audience *themselves*— which means they *don't* feel served by indie rock's "democratization" of music.
 
-Instead, they long to receive— and thus send!— honest signals once again. But how? Why, by leaving… for BYCombo! Because what more can verify your worth, than to sacrifice a serious chunk of lifetime for it?
+Instead, they long to receive— and thus send!— honest signals once again. But how? Why, by leaving… for BYCombo! Because what better way to verify your worth, than by sacrificing a serious chunk of lifetime for it?
 
 ### ${++sectionCounter}. Hustlers lack a theory of future minds
 
@@ -289,7 +289,7 @@ Like indie rock, poptimism lets the underdog triumph over any outsiders— even 
 
 This… *can't* be a viable way to make history. Because what historical figures from *our* past require the same cognitive dissonance of *us*? I can only think of the Ku Klux Klan and their ilk— America's original underdogs, who fought to suppress America's [original outsiders](https://www.goodreads.com/quotes/9648466-you-ve-got-to-be-twice-as-good-to-get-half)! Which doesn't bode well for indie rock's legacy… or poptimism's future…
 
-Yet underdog movements will never go away, because to be seen as an underdog is a *privilege*— and the privileged will always find ways to benefit themselves. But as long as artists like you are hungry enough to work much harder, to be much better… then BYCombo is here to stay as well.
+Yet underdog movements will never go away, because to be seen as an underdog is a *privilege*— and the privileged will always find ways to benefit themselves. But so long as artists like you are hungry enough to work much harder, to be much better… then BYCombo is here to stay as well.
 
 ### ${++sectionCounter}. Fair practices follow the numbers
 
