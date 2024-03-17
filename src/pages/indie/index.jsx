d9dacd9f0784f711ae36764fb7e64455ad5a9371
Bennett Lin <bennettslin@gmail.com>
2024-03-16 22:57:25 -0700

    Revise countercultures section.

--- src/pages/indie/index.jsx
+++ src/pages/indie/index.jsx
@@ -313,9 +313,9 @@ Yet as W. David Marx observes in his book [*Status and Culture*](https://www.pen
 
 And this should concern us, since Radiohead… were the ultimate hobbyists! They'd never hustled in their *lives*. Upon signing to a major label based on a demo, they were told to [change their name](https://radiohead.fandom.com/wiki/On_a_Friday#:~:text=After%20a%20chance,song%20%22Radio%20Head%22.) from On a Friday— and they *did*, to no confusion, as they were so little known in the local scene. There wasn't any recourse: The indie labels didn't want them!
 
-Well, can't *today's* indies learn to sign the *next* On a Friday? No. When status *has* to come from hustling, then *hustlers* become the ones with status— and vice versa. That is to say, a culture's status hierarchy is self-reinforcing; it *can't* be upended from within. So the losers have but one recourse: to start a *counter*-culture… which just might let them be [*tomorrow's* winners](https://genius.com/Bob-dylan-the-times-they-are-a-changin-lyrics#:~:text=And%20don%27t%20speak,they%20are%20a%2Dchangin%27)!
+Well, can't *today's* indies learn to sign the *next* On a Friday? No… Once status comes from hustling, then hustlers become high status— and vice versa. In other words, a status hierarchy is self-reinforcing; it *can't* be upended from within. Which means indie rock will *always* be an "indieocracy," as its name implies— a system ruled by hustlers, who work to get themselves known.
 
-Yet no counterculture has arisen to offer shelter from indie rock, because… well: "Why counter the good guys?" Luckily, as this century's biggest loser, I've spent half my life plotting to start one! Now with BYCombo, you too can hope to be the next Radiohead, by betting it all on the craft— your "hobby."
+Still, the losers have one recourse: to start a *counter*-culture… that just might let them be [*tomorrow's* winners](https://genius.com/Bob-dylan-the-times-they-are-a-changin-lyrics#:~:text=And%20don%27t%20speak,they%20are%20a%2Dchangin%27)! Yet none has arisen, because: "Why counter the good guys?"Luckily, as this century's biggest loser, I've spent half my life plotting to start one! Now with BYCombo, you too can hope to be the next Radiohead, by betting it all on the craft— your "hobby."
 
 ### ${++sectionCounter}. Independence breeds homogeneity
 
