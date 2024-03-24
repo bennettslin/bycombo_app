c56384940947e9e358f76aa1317c6c997c7e333b
Bennett Lin <bennettslin@gmail.com>
2024-03-24 14:15:08 -0700

    Revise independence section again.

--- src/pages/indie/index.jsx
+++ src/pages/indie/index.jsx
@@ -347,7 +347,7 @@ ${'  ' || `
 
 As their party devolved into "indieocracy," many Republicans thought they'd resist; instead, most learned to embrace the hustlers who'd now taken over— perhaps to their own surprise. Likewise, every indie gatekeeper I've met with, despite being impressed by [my projects](https://digital.lib.washington.edu/researchworks/handle/1773/23566), ended up dismissing me as a hobbyist— yet some seemed genuinely surprised by their own decision.
 
-All of which is to say, no indieocracy ever *feels* right; who gets to win, and who must lose, will always *seem* off. Still, to get along… you must go along. But I had to wonder: Shouldn't I have met an indie gatekeeper by now who'd rather just *leave*, as I did? Then one day it clicked: I *haven't* met a hundred of them. I've only met with the same one— a hundred times over!
+In other words, indieocracy never *feels* right; who gets to win, and who must lose, will always *seem* off. But to get along… you must go along. Still, I had to wonder: Shouldn't I have met an indie gatekeeper by now who'd rather just *leave*, as I did? Then one day it clicked: I *haven't* met a hundred of them. I've only met with the same one— a hundred times over!
 
 To be interdependent, you need partners who are strong where you're weak, and vice versa— which means you value others who are as lopsided as you, but in different ways. Those who want independence, however, can have no real weaknesses, and thus no real strengths. They need to be well-rounded— and this leads them all to converge on a single way of thinking.
 
