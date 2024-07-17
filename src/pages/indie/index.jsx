cadd205ba0a90b7507f8ae55b9092567cd614bec
Bennett Lin <bennettslin@gmail.com>
2024-07-17 02:27:16 -0700

    Minor note.

--- src/pages/indie/index.jsx
+++ src/pages/indie/index.jsx
@@ -411,6 +411,10 @@ So Millennials simply broke the rules that Gen X *already* broke… and called *
 
 What's a "larp"? Here's my test: Could it have been a fictional movie *instead*? Let's say, a fictional Queen… How would you commission the soundtrack?! So, Queen: no larp. "Well then, isn't it *also* no larp for an indie artist to hustle, if they're using an app that changed the paradigm too *soon* to make sense in a movie?" Okay, fair enough. But in that case, isn't the real hero… the *app*?
 
+${'  ' || `
+// TODO: Revise. Don't say "care," since I say "care" in the third paragraph.
+`}
+
 In music history courses, we learn that the evolution of the piano allowed Beethoven to compose sonatas of unprecedented [range and dynamics](https://www.cpr.org/2020/09/19/he-wanted-larger-and-louder-the-pianos-of-beethovens-time-werent-enough-for-him). By contrast, it's hardly mentioned that the invention of lithography allowed his scores to be sold to the middle class. In other words, music historians care about technology when it advances the *art form*— not the *supply chain*.
 
 Unlike what the indie camp believes, then, music history *won't* care how the Internet empowered artists to market and distribute their own works. But it *might* care how BYCombo allowed them to form bands with unprecedented lineups. So come explore one of rock's final frontiers— by having a dancer, illustrator, or puppeteer as your bandmate!
