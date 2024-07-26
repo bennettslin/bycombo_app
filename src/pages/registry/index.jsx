a46fe581deb19ba5c23400c7602657f9fc6c9b98
Bennett Lin <bennettslin@gmail.com>
2024-07-26 15:01:58 -0700

    Keep working on registry page.

--- src/pages/registry/index.jsx
+++ src/pages/registry/index.jsx
@@ -15,20 +15,16 @@ const Component = () => (
             body: [
                 `
 ${'  ' || `
-// TODO: Keep working on. Make last sentence so it's more about *needing* to divert time and energy. Which means the sentence can't really begin, "Knowing this…"
+// TODO: Review a few more times.
 `}
 
 To squeal or not? This is the prisoner's dilemma. Well, thanks to indie rock, today's artists face a similar decision: To hustle or not? We're all better off if *no* one hustles, of course— just like in the past. But once *any* artist hustles, they'll hog up all the attention. Which means *every* artist must now divert time and energy toward the hustle— to the detriment of the arts.
 
-BYCombo believes that solving this "artist's dilemma" is the key to ending the cultural stagnation of our times.
+At BYCombo, we believe this "artist's dilemma" is the reason for the cultural stagnation of our times. To solve it, a generation of artists must now martyr themselves by doubling down to make art, for untold years of their lifetime, until business partners, discerning listeners, and future historians finally give them a chance. Here's how:
 
-Business partners, discerning listeners, and future historians:
+**Each BYCombo band promises *not* to hustle, but instead to earn your trust by sacrificing a serious chunk of lifetime— at least 25,000 hours, or twelve full-time years— on a demo that advances the art form.**
 
-**To earn your trust, each band in BYCombo's public registry sacrificed a serious chunk of lifetime— at least 25,000 hours, or twelve full-time years!— on a demo that advances the art form.**
-
-But remember, since our bands aspire to make canonical art for the *future*— they'll struggle, on their own, to build a fanbase in the *present*. So indie rock's gatekeepers, along with its allies— the [poptimists](/indie#underdogs-are-supremacists-in-effect) and [tech platforms](/indie#fair-practices-follow-the-numbers)— will dismiss them as hobbyists! If rock is to be saved, then, we need *others* to give them a chance. Can one of these rock saviors… be you?
-
-If you have an offer or opportunity for any of our bands to consider, please contact them directly!
+Remember, the [indie gatekeepers](/indie#hustlers-lack-a-theory-of-future-minds), [poptimists](/indie#underdogs-are-supremacists-in-effect), and [tech platforms](/indie#fair-practices-follow-the-numbers)— each for their own reasons— are *determined* to keep the artist's dilemma in place. If rock is to be saved, then, we need *others* to step up. Can one of these saviors be… you? Please contact any one of our bands directly if you have an offer or opportunity for them to consider!
                 `,
                 (
                     <DemoLink {...bobtailYearlings} />
