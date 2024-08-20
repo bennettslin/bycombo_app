aa0d19d95fc2c0de058f8c1cd17ce6b4bb1e2874
Bennett Lin <bennettslin@gmail.com>
2024-08-20 16:02:17 -0700

    Review modest standing section.

--- src/pages/indie/index.jsx
+++ src/pages/indie/index.jsx
@@ -173,15 +173,11 @@ But this can't happen if you're always context switching— which explains why "
 
 ### ${++sectionCounter}. Modest standing leads to risk aversion
 
-In his book [*Originals*](https://adamgrant.net/book/originals), Adam Grant points out that in any hierarchy, the ones eager to take risks are those securely perched at the top, as well as those at the bottom with nothing to lose. The ones in the middle, by contrast, are the most risk-*averse*. Which explains why indie rock is so unoriginal: Its artists *seek* greater standing while its labels *cede* it— so *no* one takes any risks!
+In his book [*Originals*](https://adamgrant.net/book/originals), Adam Grant points out that in any hierarchy, the ones eager to take risks are those securely perched at the top, as well as those at the bottom with nothing to lose. The ones in the middle, by contrast, are the most risk-*averse*. Which explains why indie rock is so unoriginal: Its artists *seek* greater standing while its labels *cede* it— so *no one* takes any risks!
 
 Historically, artists lacked all means to engage with the public on their own, freeing them to explore at will— and this sometimes led to unexpected ideas. Today, however, indie artists follow all the tricks to gain fans from day one— thus leaving them reluctant to experiment, for fear of losing those fans.
 
-${'  ' || `
-// TODO: Review.
-`}
-
-Meanwhile, the major labels in the past had the budget to cast a wide net— which is, of course, the smartest *business model*, given that a [diverse roster](https://www.investopedia.com/investing/importance-diversification) enabled them to take greater risks, and thus reap [greater rewards](https://www.investopedia.com/terms/r/riskreturntradeoff.asp). Yet it also allowed them to deliver the boldest *art*— by freeing them to sign unproven artists with unexpected ideas.
+Meanwhile, the major labels in the past had the budget to cast a wide net— which is, of course, the smartest *business model*, given that a [diverse roster](https://www.investopedia.com/investing/importance-diversification) enables them to take greater risks, and thus reap [greater rewards](https://www.investopedia.com/terms/r/riskreturntradeoff.asp). Yet it also allows them to deliver the boldest *art*— by freeing them to sign unproven artists with unexpected ideas.
 
 By contrast, the indie labels sign just one artist at a time, so they go with the safe bet, *every* time, of simply picking what the [people want](https://www.goodreads.com/quotes/988332). But people only want what they know to expect— thus creating an echo chamber! This is why BYCombo tells you to explore and experiment, while you have no fans to lose: It makes you the *smart* bet— for those perched high enough to see it.
 
