316d5c0370e9e72c8b1765cee4ed7984e66397c3
Bennett Lin <bennettslin@gmail.com>
2024-10-06 12:32:36 -0700

    Revise strivers section.

--- src/pages/indie/index.jsx
+++ src/pages/indie/index.jsx
@@ -424,10 +424,10 @@ See, us dreamers, we're not grounded in reality— which makes us awful at fulfi
 Yet… it's *because* the dreamer is unrealistic that their vision, if ever fulfilled, might change the world in ways a striver's goal never can. And this is what *Millennials* don't realize— as strivers themselves. Which is why they *also* fail to see how past generations of dreamers raised the bar for innovation… such that, now, they think they'll get points just for trying!
 
 ${'  ' || `
-// TODO: Revise. Avoid saying "today" twice.
+// TODO: Review.
 `}
 
-But all is assumed to be well, since today's artists *do* resemble the innovative artists of yesteryear… while we're never shown what true innovation today would *actually* entail. Still, what if *you're* here to truly innovate— not just to "larp," or role-play, the *idea* of doing so? Then keep dreaming like a child, and let BYCombo help you find the perfect business partner: a striver!
+But all is assumed to be well, since today's artists *do* resemble the innovative artists of yesteryear… while we're never shown what true innovation should *actually* entail. Still, what if *you're* here to truly innovate— not just to "larp," or role-play, the *idea* of doing so? Then keep dreaming like a child, and count on BYCombo to help you find the perfect business partner: a striver!
 
 ### ${++sectionCounter}. Scarcity teaches generational courage
 
