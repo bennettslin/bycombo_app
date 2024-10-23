c357a9bbfac245e4ae7f955b31d4b9a1a0a9f5ff
Bennett Lin <bennettslin@gmail.com>
2024-10-23 16:18:22 -0700

    Revise registry page.

--- src/pages/registry/index.jsx
+++ src/pages/registry/index.jsx
@@ -15,16 +15,16 @@ const Component = () => (
             body: [
                 `
 ${'  ' || `
-// TODO: Revise. Just link to prisoner's dilemma and vicious circle.
+// TODO: Review a few more times.
 `}
 
-To squeal or not? This is the well-known "prisoner's dilemma." Well, thanks to indie rock, today's artists face a similar decision: To hustle or not? We're all better off if *no one* hustles to get themselves known, of course. But once *any* artist hustles, they'll hog up all the attention. Which means *every* artist must now waste time and energy on the hustle— to the detriment of the arts.
+The "indiefication" of the arts has created both a "[prisoner's dilemma](/indie#democratization-causes-noise-pollution)" and a [vicious circle](/indie#paid-dues-shrivel-the-noblest-spirits)— a prisoner's dilemma, in that any artist who redirects 5% of their effort toward *getting* the job, away from simply *deserving* it, will then force all others to do the same— and a vicious circle, in that any artist who now redirects *10%*… well, you get the picture.
 
-At BYCombo, we believe this "artist's dilemma" is the reason for the cultural stagnation of our times. And to solve it? A generation of artists must now martyr themselves by doubling down to make art for untold years, until you— as a business partner, discerning listener, or future historian— feel confident to give them a chance. Here's how:
+At BYCombo, we believe that indiefication is the reason for the cultural stagnation of our times. And to solve it? A generation of artists must now martyr themselves by doubling down to make art for untold years, until you— as a business partner, discerning listener, or future historian— feel confident enough to give them a chance. Here's how:
 
 **To earn your trust, BYCombo bands promise *not* to get themselves known. Instead, each one will sacrifice a serious chunk of lifetime— 25,000 hours, or twelve full-time years— to make a demo that advances the art form.**
 
-But remember, the [indie tastemakers](/indie#hustlers-lack-a-theory-of-future-minds), [poptimists](/indie#underdogs-are-supremacists-in-effect), and [tech platforms](/indie#fair-practices-follow-the-numbers) each have their reasons to keep the artist's dilemma in place. So if the arts are to be saved, we need *others* to step up. Can one of these saviors be you? Please contact any of our bands directly if you have an offer or opportunity for them to consider!
+But remember, the [indie tastemakers](/indie#hustlers-lack-a-theory-of-future-minds), [poptimists](/indie#underdogs-are-supremacists-in-effect), and [tech platforms](/indie#fair-practices-follow-the-numbers) each have their reasons to maintain the status quo. To save the arts, then, we need *others* to step up. Can one of these saviors be… you? Please contact any of our bands directly if you have an offer or opportunity for them to consider!
                 `,
                 (
                     <DemoLink {...bobtailYearlings} />
