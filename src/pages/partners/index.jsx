0eb4a5429ce5224409c99421bbad6bea8a797111
Bennett Lin <bennettslin@gmail.com>
2024-10-25 20:37:21 -0700

    Revise partners page.

--- src/pages/partners/index.jsx
+++ src/pages/partners/index.jsx
@@ -18,13 +18,9 @@ ${'  ' || `
 
 As a budding entrepreneur or discerning curator, would you love to partner with today's artists— if only they didn't waste so much time and energy on the *hustle*? The Beatles relied on managers from [day one](https://en.wikipedia.org/wiki/Allan_Williams), allowing them to focus entirely on the *craft*. And if history's greatest rock band was a paragon of *inter*-dependence… then how good can any *indie*-pendent artist be?
 
-Well, there's good news! The artists trying to make the best art today *don't* wish to be indie; they *do* [want others](/indie#disintermediation-is-a-middlemans-dream) to handle the business side of things. It's just that this ideal then forces them to remain unknown, both to you and the public. Or, it *did*… until now! Because here's what BYCombo offers:
+Well, there's good news! The artists trying to make the best art today *don't* wish to be indie; they *do* [want others](/indie#disintermediation-is-a-middlemans-dream) to handle the business side of things. It's just that this ideal then forces them to remain unknown to you— or, it *did*. Because now, with BYCombo, you can grant them amnesty! Here's how:
 
-${'  ' || `
-// TODO: Revise. Be clear about amnesty for what.
-`}
-
-**Every band of ours promises to put *all* their effort into advancing the art form, for a serious chunk of lifetime— while relying solely on partners like you to grant them amnesty.**
+**Every band of ours promises to put *all* their effort into advancing the art form, for a serious chunk of lifetime— while relying solely on partners like you to make them known to the public.**
 
 To hear the latest demos from our bands, you can sign up for the newsletter, or simply visit our demo registry. And remember, you're free to contact any BYCombo band directly; there are no fees whatsoever!
             `,
