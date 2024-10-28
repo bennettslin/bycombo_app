9e1fb3a6a5b7757009705187c34879a80a2af431
Bennett Lin <bennettslin@gmail.com>
2024-10-28 00:14:22 -0700

    Keep working on home page.

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -44,14 +44,18 @@ By *not* empowering myself, however, I'm dismissed as a hobbyist— and then my 
 And that's the funny part: Rock *was* interdependent once! Well then, can't a band simply promise to make the best art, as before? Sadly, no. In a world of charitable support, all art is now *subjective*; talent predicts nothing. Which means an indie label's only trusted measure is the *objective* size of an artist's fanbase. Good grief… Can we fix this mess?
 
 ${'  ' || `
-// TODO: Review next three paragraphs.
+// TODO: Revise. This one can still be phrased better.
 `}
 
-Yes! You see, indie rock has a glaring weakness: It doesn't *actually* select for the best artists. It picks "serious" artists over "casual" ones— in the faith that this amounts to the same thing. Yet, taken to its logical extreme, this would result in some of our *best* artists being classified as hobbyists! And here's how we'll prove it:
+Yes! You see, indie rock has a glaring weakness: It doesn't *actually* select for the best. It simply separates "serious artists" from "casual hobbyists"— in the faith that this amounts to the same thing. But… there's an obvious way to prove that it doesn't:
+
+${'  ' || `
+// TODO: Review next two paragraphs.
+`}
 
 **Interdependent bands must sacrifice *all* their time and energy, for untold *years* of their lifetime, on a demo that *objectively* advances the art form— and which couldn't have been made with *any* less effort.**
 
-But who'd sign up to be the first of these new rock martyrs? Why, a hungry outsider, that's who! As such, I spent 25,000 hours, or twelve full-time years, making [*Yearling's Bobtail*](https://yearlingsbobtail.com)— which includes coding and illustrating the audio webcomic. And of course, it was *only* by breaking indie rock's rules that I was able to do so. Surely, then, *someone* would grant me amnesty!
+But who'd sign up to be the first of these new rock martyrs? Why, a hungry outsider, that's who! As such, I spent 25,000 hours, or twelve full-time years, making [*Yearling's Bobtail*](https://yearlingsbobtail.com)— which includes coding and illustrating the audio webcomic. And of course, it was *only* by breaking indie rock's rules that I was able to do so. Surely, then, I'd be given amnesty!
 
 ### Who makes the tastemakers?
 
@@ -73,19 +77,27 @@ Worst of all, if the major labels are "bad guys," then this makes the indies "go
 
 ### A hundred *Yearling's Bobtail*s
 
-If we can't resist, though— let's revolt! How? We… wait. See, indie rock has *another* glaring weakness: In judging an artist by the size of their fanbase, its tastemakers end up favoring *content*— namely, art that's optimized to gain fans in the *present*, at the cost of having none in the *future*. But content has little chance to make history… and to prove this, we just need time to pass.
-
 ${'  ' || `
-// TODO: Revise next two paragraphs. Move idea of some artists being punished for having broken indie rock's rules to first paragraph, making it easier for second paragraph to begin with "Dr. Dres of rock."
+// TODO: Review.
 `}
 
-Sure enough, the last two decades of rock have had [little impact](https://today.yougov.com/entertainment/articles/36462-best-decade-for-music-americans-poll-data); even Gen Z mostly agrees! Which is the first step to reviving interdependence ourselves, since it's only once the *youth* are discontent that budding entrepreneurs and discerning curators will try to disrupt the market.
+If we can't resist, though— let's revolt! How? We… wait. See, indie rock has *another* glaring weakness: In judging artists by the size of their fanbase, its tastemakers end up favoring *content*— namely, art that's optimized to gain fans in the *present*, at the cost of having none in the *future*. But content has little chance to make history… and to prove this, we just need time to pass.
 
-To those who've had to remain unknown for having broken indie rock's rules, these "Dr. Dres of rock" would love to give amnesty. But how, if they're unknown? Low barriers to entry have led to a hopelessly crowded field. The next step, then, is for [elitist gatekeepers](/indie#elitism-is-the-great-equalizer) like BYCombo to enforce a steep *cost* of entry. And here's our approach:
+${'  ' || `
+// TODO: Revise. Last sentence can be phrased better.
+`}
 
-**To have a demo shown in our registry, each BYCombo band promises *not* to empower themselves. Instead, they'll earn a partner's trust by sacrificing a serious chunk of lifetime to advance the art form.**
+Sure enough, the last two decades of rock have had [little impact](https://today.yougov.com/entertainment/articles/36462-best-decade-for-music-americans-poll-data); even Gen Z seems to agree! Which is the first step to reviving interdependence, since it's only once the *youth* are discontent that budding entrepreneurs will try to disrupt the market— and discerning curators, to reform the system.
 
-So picture a *hundred* BYCombo bands, all of them committed to making the historically consequential works of our times. This is the *final* step to reviving interdependence— the next revolution in rock history!
+${'  ' || `
+// TODO: Review next three paragraphs.
+`}
+
+These "Dr. Dres of rock" would love to give amnesty to interdependent bands who've broken indie rock's rules, I'm sure! But how? Any such band currently remains unknown for having done so, and today's field is more crowded than ever. The next step, then, is for an [elitist gatekeeper](/indie#elitism-is-the-great-equalizer) like BYCombo to enforce a steep *cost* of entry. In our case, it works like so:
+
+**Every BYCombo band must promise *not* to empower themselves. Instead, they'll earn a partner's trust by sacrificing a serious chunk of lifetime to make a demo that advances the art form.**
+
+So picture a *hundred* BYCombo bands, if you will, all of them set on making the historically consequential works of our times. This will be the *final* step to reviving interdependence— the next revolution in rock history!
             `,
         }}
     />
