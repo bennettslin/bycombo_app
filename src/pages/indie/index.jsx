9c0b47239fa97b0531efd3e36aecdb559f448b55
Bennett Lin <bennettslin@gmail.com>
2024-12-27 14:21:34 -0800

    Revise establishment section.

--- src/pages/indie/index.jsx
+++ src/pages/indie/index.jsx
@@ -326,18 +326,16 @@ ${'  ' || `
 
 Probably best between elitism and counterculture sections.
 
-Third paragraph: Discuss reason versus emotion?
-
 Fifth paragraph: Keep working on, obviously. Mention countermovement of some kind, in preparation for counterculture section.
 
 Review links to determine they're the best possible. Change New Deal link to point out that Black leaders criticized New Deal.
 `}
 
-In high school, a classmate referred to the carpetbaggers as "bad guys"— for having exploited the defeated South. This surprised me, since I saw them as "good guys"— for having defended vulnerable Blacks. Clearly, he sided with the people, while I sided with the elites and the underclass. So the "sideways horseshoe" shows up not just in culture… but in politics as well!
+In high school, a classmate referred to the carpetbaggers as "bad guys"— for having exploited the defeated South. This surprised me, since I saw them as "good guys"— for having defended vulnerable Blacks. Clearly, he sided with the people, while I sided with the elites and the underclass. So this model of a "sideways horseshoe" shows up not just in culture… but in politics as well!
 
-Now, past generations didn't need to coin this term; they intuitively grasped why the underclass should distrust populism— the [New Deal](https://www.rockefellerfoundation.org/insights/perspective/the-new-deal-made-americas-racial-inequality-worse-we-cant-make-the-same-mistake-with-covid-19-economic-crisis/#:~:text=It%E2%80%99s%20true%20that,Democrats%20in%20Congress.), anyone?— but we've since lost this intuition. As a result, progressive Millennials keep trying to pit *all* of us against the political establishment… only to watch in confusion as the most disadvantaged among us side with the [elites instead](https://www.nbcnews.com/politics/elections/2016-clinton-sanders-black-voters-pick-winner-n1029631).
+Now, our forebears didn't need to coin such a term; they intuitively grasped why the underclass should distrust populism— the [New Deal](https://www.rockefellerfoundation.org/insights/perspective/the-new-deal-made-americas-racial-inequality-worse-we-cant-make-the-same-mistake-with-covid-19-economic-crisis/#:~:text=It%E2%80%99s%20true%20that,Democrats%20in%20Congress.), anyone?— but we've since lost this intuition. As a result, progressive Millennials keep trying to pit *all* of us against the political establishment… only to watch in confusion as the most disadvantaged among us side with the [elites instead](https://www.nbcnews.com/politics/elections/2016-clinton-sanders-black-voters-pick-winner-n1029631).
 
-Of course, it isn't just in politics, but also in culture, where this confusion has led Millennials to be so ineffectual. As a minority, I've long understood that my best bet is to win over a few gatekeepers, rather than the countless many at the grassroots. Yet the indie camp— their good guys— continue to insist that I'm voting against my own interests…
+Of course, it isn't just in politics, but also in culture, where this confusion has made Millennials so ineffectual. For instance, as a minority artist, I do better trying to win the minds of a few elites, rather than the hearts of many at the grassroots. Yet the indie camp— their generation's good guys— simply won't hear it, insisting that I'm "voting against my own interests."
 
 Which explains why both culture *and* politics have gotten worse in our times: In each case, Millennials have mistaken populism for progressivism— when there's a pretty clear difference! A populist *patronizes* the disadvantaged for wanting to join the establishment, whereas a progressive *sympathizes*— and thus sees wisdom in joining as well.
 
