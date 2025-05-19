9e69d12b3994d9c18931f44ecc393d8db7b0457d
Bennett Lin <bennettslin@gmail.com>
2025-05-19 12:16:30 -0700

    Revise interdependence paragraphs.

--- src/pages/indie/index.jsx
+++ src/pages/indie/index.jsx
@@ -69,9 +69,17 @@ To make history, a band must be exceptionally strong in the ways of art. But thi
 
 **Why interdependence?**
 
-Partners are most impactful when helping to make you known as an artist— to take you "from zero to one." But an indie tastemaker holds off *until* you're known, and then helps to get the public to care— to take you "from one to *n*." Yet, by that point, their business skills have far less impact…
+${'  ' || `
+// TODO: Review today.
+`}
 
-Meanwhile, your artistic talents work the *opposite* way: They get you far once you're *already* known; where they stumble, though, is in getting you known to *begin* with. Yet it's the latter effort— a sign of your *indie*-pendence— that an indie tastemaker needs to see! Which raises the question: If your partner can't grasp the benefits of *inter*-dependence… are they even a partner at all?
+Partners are most impactful when helping to make you known as an artist— to take you "from zero to one," as they say. But an indie tastemaker holds off *until* you're already known, then helps to get the public to care— to take you "from one to *n*." Yet, by that point, their business skills have far less impact…
+
+${'  ' || `
+// TODO: Review today.
+`}
+
+Meanwhile, your artistic talents work the *opposite* way: They get you far once you're *already* known; where they stumble, though, is in getting you known to *begin* with. Yet it's the latter effort— a sign of your *indie*-pendence— that an indie tastemaker needs to see! Which raises the question: If your partner can't grasp the benefits of *inter*-dependence… are they a partner at all?
 
 **Why demos?**
 
