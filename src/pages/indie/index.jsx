d518357d17f4f5e0b4fe948bc392e8419a6f0f0e
Bennett Lin <bennettslin@gmail.com>
2025-06-07 12:04:29 -0700

    Morning reviews.

--- src/pages/indie/index.jsx
+++ src/pages/indie/index.jsx
@@ -219,17 +219,9 @@ Consequently, it's often those with the [*least* experience](https://youtu.be/AC
 
 Ironically, the indie labels still enjoy a *reputation* for giving us the boldest art! The trick? When a work intentionally *lacks* enviable hooks, it wins praise for challenging the listener. Yet past artists would challenge us… *and* inspire our envy! In hindsight, then, this century's indie rock doesn't *feel* like a historical era, so much as a "larp"— or, live-action role-play— of one.
 
-${'  ' || `
-// TODO: Review today.
-`}
-
 Why do the indies default to larping now? Basically, they're overworked and underpaid— and this is exactly their selling point: By *not* hiring middlemen, they pass the savings onto their artists. Then, to conserve "bandwidth," they simply follow the instincts handed down by their forebears… just like moths, which evolved to [orient themselves](https://www.theguardian.com/science/2024/jan/30/why-are-moths-attracted-to-lights-science-answer#:~:text=moths%20and%20many,they%20flew%20level.) by starlight.
 
 Of course, these instincts made sense in the '80s, when underground bands would build a fanbase by playing local shows. It's precisely *because* word of mouth only traveled locally, for example, that the indies came to trust it so highly. And likewise with their respect for direct fan engagement: Back then, it was only *ever* possible with a local gathering.
-
-${'  ' || `
-// TODO: Review today.
-`}
 
 But today, algorithms have turned word of mouth into wisdom of the crowd— which settles on an average by canceling out all extremes. And social media has made engaging with fans trivial; those who do so now might enjoy *fame*, but never *acclaim*. Yet the indies haven't noticed! Like moths circling around a flame, they're stuck with outdated instincts in a modern world.
 
@@ -561,6 +553,8 @@ So if I could get every progressive— in both culture and politics— to read j
 
 ${'  ' || `
 // TODO: Keep working on.
+
+* Move this and consequentialists section between scarcity and unity sections?
 
 * In this section or the next, point out that Gen X allowed indie rock to coexist as a counterculture to alternative in the '90s, but Millennials refused to allow interdependence to coexist with indie rock in the '00s?
 
