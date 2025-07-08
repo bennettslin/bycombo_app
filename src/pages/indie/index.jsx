4d39e284b293f0e3ec7b684385baa6567d64f95c
Bennett Lin <bennettslin@gmail.com>
2025-07-07 19:33:12 -0700

    Revise bandwidth section again.

--- src/pages/indie/index.jsx
+++ src/pages/indie/index.jsx
@@ -231,7 +231,11 @@ Of course, these instincts made sense in the '80s, when underground bands would 
 
 But today, algorithms have turned word of mouth into wisdom of the crowd— which settles on an average by canceling out all extremes. And social media has made engaging with fans trivial; those who do so now might enjoy *fame*, but never *acclaim*. Yet the indies haven't noticed! Like moths circling a flame, they're stuck with outdated instincts in a modern world.
 
-So they keep striving to give us the next "[Range Life](https://youtu.be/1VVj1zqbWpU)"— that is, the next song whose context we'll lap up, out of loyalty to the artist who made it— as if this isn't how *all* music works today! Meanwhile, what's rare now is for bands to craft radio-friendly hooks envied by others… yet critics *still* call it "punching up" to bash them— and even when inspiring envy is *key* to making history!
+${'  ' || `
+// TODO: Review.
+`}
+
+So they keep striving to give us the next "[Range Life](https://youtu.be/1VVj1zqbWpU)"— that is, the next song whose context we'll lap up, out of loyalty to the artist who made it— as if this isn't how *all* music works today! Meanwhile, what's rare now is for bands to craft radio-friendly hooks envied by others… yet it's *still* seen as "punching up" to bash them— and even when inspiring envy is *key* to making history!
 
 Which explains why this century's indie rock doesn't *feel* like a historical era, so much as a "larp"— or, live-action role-play— of one. Sadly, with *every* label on a tight budget these days, even the majors are just larping now. But what if you're here to *actually* make history? Then come join BYCombo… and find a partner with enough bandwidth to change with the times!
 
