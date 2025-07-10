3199f97d4d8da7bb8582a7ec940d5856ad64f6ed
Bennett Lin <bennettslin@gmail.com>
2025-07-10 08:44:53 -0700

    Revise demos page.

--- src/pages/demos/index.jsx
+++ src/pages/demos/index.jsx
@@ -18,7 +18,11 @@ At BYCombo, we believe that "[indiefication](/indie#the-simplest-explanation-is-
 
 **Every band in this demo registry has pledged *not* to get themselves known. Instead, they've each sacrificed a serious chunk of lifetime— 25,000 hours, or twelve full-time years— to make a demo that advances the art form.**
 
-But remember, the indie tastemakers and their allies— the "[poptimists](/indie#underdogs-are-supremacists-in-effect)" and the [tech platforms](/indie#fair-practices-follow-the-numbers)— each have their reasons to keep indiefication going. To save the arts, then, we need *others* to step up. Can one of our saviors… be you? If you have an opportunity or offer for any BYCombo band to consider, please contact them directly!
+${'  ' || `
+// TODO: Review.
+`}
+
+But remember, the indie tastemakers and their allies— the "[poptimists](/indie#underdogs-are-supremacists-in-effect)" and the [tech platforms](/indie#fair-practices-follow-the-numbers)— each have their reasons to keep indiefication going. To save the arts, then, we need *others* to step up. Can one of these saviors… be you? If you have an opportunity or offer for any BYCombo band to consider, please contact them directly!
                 `,
                 (
                     <DemoLink {...bobtailYearlings} />
