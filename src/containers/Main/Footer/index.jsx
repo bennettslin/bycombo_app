1247eb9b737e75916833d284c54bb29151fb11a4
Bennett Lin <bennettslin@gmail.com>
2025-09-16 14:49:42 -0700

    Remove upsell from footer.

--- src/containers/Main/Footer/index.jsx
+++ src/containers/Main/Footer/index.jsx
@@ -1,18 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import cx from 'classnames'
-import { useSelector } from 'react-redux'
 import { CSSTransition } from 'react-transition-group'
 import Flex from '../../../components/Flex'
 import MailingList from '../../../components/MailingList'
-// import FollowButtons from '../../../components/FollowButtons'
 import StyledFrame from '../StyledFrame'
-import { mapIsUpsellShown } from '../../../redux/option/selector'
 import './style'
 
 const MainFooter = () => {
-    const
-        isUpsellShown = useSelector(mapIsUpsellShown),
-        [didMount, setDidMount] = useState(false)
+    const [didMount, setDidMount] = useState(false)
 
     useEffect(() => {
         setTimeout(() => setDidMount(true), 0)
@@ -22,7 +17,7 @@ const MainFooter = () => {
         <CSSTransition
             appear
             {...{
-                in: didMount && !isUpsellShown,
+                in: didMount,
                 timeout: 200,
                 classNames: {
                     enterDone: 'shown',
@@ -41,13 +36,11 @@ const MainFooter = () => {
                         className: cx(
                             'MainFooter',
                         ),
-                        // justifyContent: 'spaceBetween',
                         gap: 'sm',
                     }}
                 >
 
                     <MailingList isWide />
-                    {/* <FollowButtons /> */}
                 </Flex>
             </StyledFrame>
         </CSSTransition>
