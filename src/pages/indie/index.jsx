b0053db6d39a2794d61bc9453bd14dc4fec812e6
Bennett Lin <bennettslin@gmail.com>
2025-09-21 12:01:12 -0700

    Revise hungry outsiders section.

--- src/pages/indie/index.jsx
+++ src/pages/indie/index.jsx
@@ -328,7 +328,13 @@ Which is why it took an outsider like myself to start BYCombo, driven by an urge
 
 "Who could've foreseen all the drawbacks of propagating indie rock's ideals?" Um… us women and minorities, that's who! If history's any guide, we *need* to be the best, just to be heard at all— and thus we *can't* be self-reliant. Rappers get this, which is why they rely on [labels and producers](http://archive.today/2024.05.11/https://genius.com/Dr-dre-nuthin-but-a-g-thang-lyrics#:~:text=Ain%27t%20nothin%27%20but,that%20pays%20me). Yes, they'll boast of having hustled— but it's to say, "I've got cred *too*… so yours means *nothing*."
 
+${'  ' || `
+// TODO: Review a few times.
+
 Yet in spite of the poor fit, indie rock sure is eager lately to hand us the reins! Why is this? Because, after years of "larping," or live-action role-playing, as an era-defining movement— despite being no better than a foreign canon— it's in *decline*… and desperate to regain its cultural relevance. So we women and minorities are being nudged onto a "[glass cliff](https://www.investopedia.com/terms/g/glass-cliff.asp)."
+`}
+
+Yet in spite of the poor fit, indie rock sure is eager lately to hand us the reins! Why? It's because "larping," or live-action role-playing, is good enough only when aiming for a *foreign* canon, not "the" canon— and after years of larping as an era-defining movement, the indie camp has lost all cultural relevance. So now, we women and minorities are being nudged onto a "[glass cliff](https://www.investopedia.com/terms/g/glass-cliff.asp)"…
 
 No thanks! Remember, the indie camp idolizes those like Stephen Malkmus and Jeff Mangum, whose privileges as society's underdogs allow them to live *devoid* of hunger. Yet rock and roll was founded by poor, queer Blacks, then carried further by rural Southerners, a Midwestern Jew, working-class Brits… In short, it's a history of outsiders, like us— and specifically, the *hungriest*.
 
