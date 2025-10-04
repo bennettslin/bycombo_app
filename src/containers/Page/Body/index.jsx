cba037726bc599ad435205eb04231af873df04f0
Bennett Lin <bennettslin@gmail.com>
2025-10-04 12:42:13 -0700

    Begin changing responsive widths.

--- src/containers/Page/Body/index.jsx
+++ src/containers/Page/Body/index.jsx
@@ -26,6 +26,7 @@ const Body = () => {
             {...{
                 className: cx(
                     'Body',
+                    'fontSize__md',
                 ),
             }}
         >
