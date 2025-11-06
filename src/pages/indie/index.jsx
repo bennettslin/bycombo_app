724e9326205e40f4d54fa52b115f232cb4b2bb80
Bennett Lin <bennettslin@gmail.com>
2025-11-06 15:24:32 -0800

    Revise supremacists section yet again.

--- src/pages/indie/index.jsx
+++ src/pages/indie/index.jsx
@@ -429,13 +429,13 @@ ${'  ' || `
 // TODO: Review. Also review link.
 `}
 
-Given [their disdain](https://www.avclub.com/stone-temple-pilots-theyre-actually-good-really-1798213754#:~:text=In%20spite%20of,of%20mainstream%20rock.) for well-crafted hooks, the indie camp cheered when the Internet killed off the alternative rock I grew up on… only to watch as young folks simply switched to pop's *market-driven* hooks— rather than join them in going without hooks at all! Unfazed, though, they then made the switch as well, bringing their good intentions along. And so "poptimism" was born…
+Given [their disdain](https://www.avclub.com/stone-temple-pilots-theyre-actually-good-really-1798213754#:~:text=In%20spite%20of,of%20mainstream%20rock.) for well-crafted hooks, the indie camp cheered when the Internet killed off the alternative rock I grew up on… only to watch as young folks then switched to pop's *market-driven* hooks— rather than join them in going without hooks at all! Unfazed, though, they simply made the switch as well, bringing their good intentions along. And so "poptimism" was born…
 
 ${'  ' || `
 // TODO: Review.
 `}
 
-"Wait, indie rock gave birth to poptimism?!" It sounds odd, I know! But think about it: Both are underdog movements that supplanted an earlier culture founded by outsiders. For the former, it was Sister Rosetta Tharpe's rock and roll; and for the latter, the classic pop of Tin Pan Alley's Jewish immigrants.
+"Wait, indie rock gave birth to poptimism?" It sounds odd, I know! But think about it: Both are underdog movements that supplanted an earlier culture founded by outsiders. For the former, it was Sister Rosetta Tharpe's rock and roll; and for the latter, the classic pop of Tin Pan Alley's Jewish immigrants.
 
 ${'  ' || `
 // TODO: Review.
