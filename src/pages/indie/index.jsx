34afc28c5c635035f1ab899be7089e578efac9a6
Bennett Lin <bennettslin@gmail.com>
2025-12-10 17:49:02 -0800

    Revert change.

--- src/pages/indie/index.jsx
+++ src/pages/indie/index.jsx
@@ -347,11 +347,7 @@ The issue is that these labels are run by society's most privileged groups— an
 
 So far, though, their efforts have only helped women, not us minorities— or *male* minorities, anyway. Why? Because our struggles *aren't* alike: Minorities [face stereotypes](https://en.wikipedia.org/wiki/Stereotype_content_model) of low *warmth*— and women, of low *competence*. But social justice, when self-enforced, *only* helps those misjudged as incompetent; by contrast, a circle of friends has every *right* to feel warmest toward its own.
 
-${'  ' || `
-// TODO: Review.
-`}
-
-Still, if our kind *isn't* desired as friends… we might be valued as *investments*! After all, a [smart investor](https://genius.com/Eminem-white-america-lyrics#:~:text=When%20I%20was,under%20his%20ass) buys low and sells high— and in music, this means betting on us low-status outsiders with history-making talents. So leave the indie camp to their "old-boy network"; with BYCombo, you can partner with a capitalist who'll happily assure you, "Yes, I'm in it for the money!"
+Yet if our kind *isn't* desired as friends… we can still be valued as *investments*! After all, a [smart investor](https://genius.com/Eminem-white-america-lyrics#:~:text=When%20I%20was,under%20his%20ass) buys low and sells high— and in music, this means betting on us low-status outsiders with history-making talents. So leave the indie camp to their "old-boy network"; with BYCombo, you can partner with a capitalist who'll be happy to assure you, "I'm *totally* in it for the money!"
 
 ### ${++sectionCounter}. Elitism is the great equalizer
 
