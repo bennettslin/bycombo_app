49cde6e7335896f0bf1ab8380e49a1f87d07a5e9
Bennett Lin <bennettslin@gmail.com>
2026-01-16 11:19:47 -0800

    Revise apply page.

--- src/pages/apply/index.jsx
+++ src/pages/apply/index.jsx
@@ -70,11 +70,15 @@ Email us the following information:
 
 We'll also need each of the following:
 
+${'  ' || `
+// TODO: Review.
+`}
+
 - A statement that you're of legal age: eighteen years or older.
-- Your gender. (This only determines whom we'll place you in a band with. It *doesn't* affect your chances of being accepted.)
-- Total hours you spend each week to earn a living, including commute time. We'll use this as a proxy for your economic class. (Same condition as above.)
+- Your gender. (This *only* determines whom we'll place you in a band with. It *doesn't* affect your chances of being accepted.)
+- Total hours you'll spend each week to earn a living, including commute. (We'll use this as a proxy for economic class. Also, the same condition as above applies, so just make an honest guess if you aren't sure.)
 - Whether you're able and willing to relocate to Los Angeles, for anyone who isn't here already. (If not, you won't be eligible for the pilot cohort, but we'll keep you on file for later ones.)
-- An acknowledgment that you've read "[The problem with indie rock](/indie)" in its entirety. (You don't have to agree with all of it. We just need you to understand what drives the BYCombo mission.)
+- An acknowledgment that you've read [Bennett's problem](/indie) with indie rock in its entirety. (You don't have to agree with all of it. We just need you to understand what drives the BYCombo mission.)
 
 Feel free to include any of the following as well:
 
