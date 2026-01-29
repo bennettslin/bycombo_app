e46e0f59b1d11cc925cc7311c5f4f36bfeb5a83a
Bennett Lin <bennettslin@gmail.com>
2026-01-29 11:06:17 -0800

    Revise unity section.

--- src/pages/indie/index.jsx
+++ src/pages/indie/index.jsx
@@ -499,7 +499,11 @@ I'll tell you my theory: Millennials only faced boredom long enough to hate it; 
 
 Yet my warning remains valid: "Indie rock is this generation's *glam metal*, not *alternative*." Except, today, the generation in question is *Gen Z*— who've only [ever faced](http://archive.today/2022.12.16/https://www.nytimes.com/2022/12/15/style/teens-social-media.html) an *abundance* of content… and would now *love* to revolt! Well, this Xennial offers a solution: With BYCombo, you can show courage once again— by doubling down to make art, for a serious chunk of lifetime.
 
-### ${++sectionCounter}. Unity scapegoats the lone dissident
+${'  ' || `
+// TODO: Review title.
+`}
+
+### ${++sectionCounter}. Unity scapegoats a lone dissident
 
 Back when content was scarce, joining a scene— goth, punk— was a full-time effort; it meant rejecting the rest. This taught Gen X to stand fiercely for their side… knowing that others are still [good people](https://web.archive.org/web/2012/http://www.mitchclem.com/nothingnice/196). Now for Millennials, it's the other way around: Each scene is just an "era" to try on— but any threat to the structures that allow for such "larping"… must be fiercely opposed by *all*.
 
