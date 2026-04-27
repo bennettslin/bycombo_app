17a77d3d02578f74a5653c9605fc48279fa899f7
Bennett Lin <bennettslin@gmail.com>
2026-04-27 00:38:34 -0700

    Minor fixes.

--- src/pages/highlights/their-identities.jsx
+++ src/pages/highlights/their-identities.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Page from '../../containers/Page'
 
-const TITLE = `"random boost"`
+const TITLE = `"their identities"`
 const DESCRIPTION = `Highlighted text from linked source.`
 
 const Component = () => (
