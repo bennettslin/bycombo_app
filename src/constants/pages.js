92be4e307dd60b121ba677832b22f067d29cd180
Bennett Lin <bennettslin@gmail.com>
2026-04-26 19:14:10 -0700

    Remove back to highlights link.

--- src/constants/pages.js
+++ src/constants/pages.js
@@ -8,7 +8,11 @@ export const NOT_FOUND_PAGE = '404'
 export const DEMOS_PAGE = 'demos'
 export const APPLY_PAGE = 'apply'
 export const INDIE_PAGE = 'indie'
+export const HIGHLIGHTS_PAGE = 'highlights'
 
 export const ID_LINK_KEY = 'idLink'
 
-export const TABBED_PAGES_SET = new Set([])
+// This prevents rendering of the "Back to" anchor.
+export const TABBED_PAGES_SET = new Set([
+    HIGHLIGHTS_PAGE,
+])
