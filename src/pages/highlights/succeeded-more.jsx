927fe700700be13a449b1d912427213e327cca6c
Bennett Lin <bennettslin@gmail.com>
2026-04-30 02:52:07 -0700

    Change succeeded more highlight source.

--- src/pages/highlights/succeeded-more.jsx
+++ src/pages/highlights/succeeded-more.jsx
@@ -11,9 +11,9 @@ const Component = () => (
             metaTitle: TITLE,
             metaDescription: DESCRIPTION,
             body: `
-> "So thank you, Uncle Sam. Someday your nieces and nephews at Berkshire hope to send you even larger payments than we did in 2024. Spend it wisely. Take care of the many who, for no fault of their own, get the short straws in life. They deserve better."
+> So thank you, Uncle Sam. Someday your nieces and nephews at Berkshire hope to send you even larger payments than we did in 2024. Spend it wisely. Take care of the many who, for no fault of their own, get the short straws in life. They deserve better.
 
-Littleton, Cynthia. "[Warren Buffett Says 'It Won't Be Long' Before Greg Abel Succeeds Him as Berkshire Hathaway CEO.](https://variety.com/2025/biz/news/warren-buffett-greg-abel-succeeded-ceo-berkshire-1236316306)" *Variety*, February 2025.
+Buffett, Warren. "[2024 Annual Letter to Shareholders.](https://www.berkshirehathaway.com/letters/2024ltr.pdf)" *Berkshire Hathaway*, February 2025.
             `,
         }}
     />
