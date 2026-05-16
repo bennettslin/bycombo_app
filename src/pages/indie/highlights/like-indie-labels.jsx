b8388f907eeaf3396426204c6bd294897f48846d
Bennett Lin <bennettslin@gmail.com>
2026-05-16 09:25:53 +0800

    Minor revisions.

--- src/pages/indie/highlights/like-indie-labels.jsx
+++ src/pages/indie/highlights/like-indie-labels.jsx
@@ -11,9 +11,9 @@ const Component = () => (
             metaTitle: TITLE,
             metaDescription: HIGHLIGHTS_DESCRIPTION,
             body: `
-> [R]ecord labels are now offering very flexible terms to TikTok stars. I hear that royalty rates of 50% are now possible, and even future ownership of the master recordings.
+> I hear that royalty rates of 50% are now possible, [as well as] future ownership of the master recordings.
 
-> Labels aren't doing this because they suddenly decided to be generous. They are sweetening deals because they have nothing else to offer. They are trying to sign artists who absolutely don't need a record label— because [these artists] have already gone viral and are making money as indie operators.
+> [But] labels aren't doing this because they suddenly decided to be generous; they're sweetening deals because they have nothing else to offer. They're trying to sign artists who absolutely don't need a record label— [artists who've] already gone viral and are making money as indie operators.
 
 Gioia, Ted. "[Nine Predictions for the Future of the Music Business.](https://www.honest-broker.com/p/nine-predictions-for-the-future-of)" *The Honest Broker*, October 2024.
             `,
--- src/pages/indie/highlights/need-to-quit.jsx
+++ src/pages/indie/highlights/need-to-quit.jsx
@@ -11,7 +11,9 @@ const Component = () => (
             metaTitle: TITLE,
             metaDescription: HIGHLIGHTS_DESCRIPTION,
             body: `
-> The reality is that the amount of focus and desire required to blow up the occasional building or pipeline is nothing compared to the lifetime of quiet labor required to understand the system well enough to actually build a better one. And that better world, if it arrives, will require the cooperation of some truly unpleasant people, because all of civilization is nothing but truly unpleasant people learning to peacefully cooperate.
+> The reality is that the amount of focus and desire required to blow up the occasional building or pipeline is nothing compared to the lifetime of quiet labor required to understand the system well enough to actually build a better one.
+
+> And that better world, if it arrives, will require the cooperation of some truly unpleasant people, because all of civilization is nothing but truly unpleasant people learning to peacefully cooperate.
 
 Pargin, Jason. "[Is Modern Mass Media a Mind Prison?](https://jasonpargin.substack.com/p/is-modern-mass-media-a-mind-prison)" *Jason Pargin's Newsletter*, March 2024.
             `,
