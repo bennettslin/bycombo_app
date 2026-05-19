1741b6adf96236dedc7102b859960e9ae4b8ea6a
Bennett Lin <bennettslin@gmail.com>
2026-05-19 15:12:42 +0800

    Additional revisions.

--- src/pages/indie/highlights/lends-authenticity.jsx
+++ src/pages/indie/highlights/lends-authenticity.jsx
@@ -11,7 +11,9 @@ const Component = () => (
             metaTitle: TITLE,
             metaDescription: HIGHLIGHTS_DESCRIPTION,
             body: `
-> *The guy who recorded an album alone in the woods.* This line might end up on Justin Vernon's tombstone. There's something irresistible about the thought of a bearded dude from small-town Wisconsin retreating, heartbroken, to a cabin to write some songs […] These days, Vernon is more likely to poke fun at the image, but it endures because it fulfils a fantasy for us as listeners.
+> "The guy who recorded an album alone in the woods." This line might end up on Justin Vernon's tombstone. There's something irresistible about the thought of a bearded dude from small-town Wisconsin retreating, heartbroken, to a cabin to write some songs.
+
+[Of course], Vernon is more likely to poke fun at the image— but it endures because it fulfills a fantasy for us as listeners.
 
 Richardson, Mark. "[*Bon Iver*, Bon Iver.](https://pitchfork.com/reviews/albums/15551-bon-iver)" *Pitchfork*, June 2011.
             `,
