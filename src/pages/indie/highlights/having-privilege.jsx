91ada24e7cbc5cc59746d785135587a6970fc335
Bennett Lin <bennettslin@gmail.com>
2026-05-23 09:32:30 -0700

    Revise some highlights.

--- src/pages/indie/highlights/having-privilege.jsx
+++ src/pages/indie/highlights/having-privilege.jsx
@@ -11,7 +11,7 @@ const Component = () => (
             metaTitle: TITLE,
             metaDescription: HIGHLIGHTS_DESCRIPTION,
             body: `
-> Many times when I identified as Libertarian, people said to me, "It's just rich white guys that don't want to be told what to do," and I had a zillion answers to that— and now that seems 100% accurate.
+> Many times when I identified as Libertarian, people said to me, "It's just rich White guys that don't want to be told what to do," and I had a zillion answers to that— and now that seems 100% accurate.
 
 Grierson, Tim. "[Penn Jillette Wants to Talk It All Out.](https://www.cracked.com/article_40871_penn-jillette-wants-to-talk-it-all-out.html)" *Cracked*, January 2024.
             `,
--- src/pages/indie/highlights/watch-in-confusion.jsx
+++ src/pages/indie/highlights/watch-in-confusion.jsx
@@ -15,7 +15,7 @@ const Component = () => (
 
 > Among [South Carolina's] Black voters, Clinton's margin of support was staggering: 72 percentage points, 86% to 14%. […] Blacks [also] made up more than one-quarter of all Democratic primary voters nationally, and they were instrumental in supplying Clinton with what became an insurmountable delegate lead.
 
-Kornacki, Steve. "[2016: Clinton, Sanders and black voters pick the winner.](https://www.nbcnews.com/politics/elections/2016-clinton-sanders-black-voters-pick-winner-n1029631)" *NBC News*, July 2019.
+Kornacki, Steve. "[2016: Clinton, Sanders and Black Voters Pick the Winner.](https://www.nbcnews.com/politics/elections/2016-clinton-sanders-black-voters-pick-winner-n1029631)" *NBC News*, July 2019.
             `,
         }}
     />
