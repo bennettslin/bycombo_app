6ea2a0cdccadad63e14bad6587a50130fecf67f1
Bennett Lin <bennettslin@gmail.com>
2026-05-27 21:15:56 -0700

    Work on gatekeeper commentary even further.

--- src/pages/indie/commentary/gatekeeper.jsx
+++ src/pages/indie/commentary/gatekeeper.jsx
@@ -11,19 +11,15 @@ const Component = () => (
             metaTitle: TITLE,
             metaDescription: COMMENTARY_DESCRIPTION,
             body: `
-For many people, "gatekeeping" simply refers to any selection process that disfavors them. But then they apply their *own* criteria— which necessarily disfavor *others*— leading to confusion! So let's agree that a gatekeeper isn't some bogeyman we're all united against; some of us are, in fact, on the "for" side. The question now is: Which of us, exactly?
+For many, "gatekeeping" refers to any selection process that doesn't favor them. But then they'll apply their *own* criteria— which disfavor *others*!— creating confusion all around. So let's agree that the gatekeeper isn't some universal bogeyman— that some of us stand with them, and others against. The question now is: Who's on which side?
 
-${'  ' || `
-// TODO: Keep working on. Make clear that in the absence of such a gate, we'd revert to the natural order of "might makes right."
-`}
-
-It helps if we think of a literal gate— the purpose of which is to permit those who'd otherwise be left out, while preventing others who'd otherwise march right in. To put gatekeepers in charge, then, is to *subvert* the natural order of "might makes right"— and in music, such might comes from hard numbers, like the size of your fanbase.
+It helps if we think of a literal gate— the purpose of which is to permit those who'd otherwise be shut out, while preventing others who'd march right in. Without gatekeepers, then, we'd revert to the natural order of "might makes right"— and in the arts, such might comes from hard numbers. So the indie camp, which judges you on the size of your fanbase, stands *against*.
 
 ${'  ' || `
 // TODO: Keep working on. Explicitly state that the indie camp is on the side of "might makes right."
 `}
 
-So if, like me, you *despair* over being judged by the number of fans you have, then you're *not* opposed to gatekeeping— but rather, the lack of it!
+But if, like me, you *despair* over being judged by the number of fans you have, then you're *not* opposed to gatekeeping— but rather, the lack of it!
             `,
         }}
     />
