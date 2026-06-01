7d6381f738a68e1ac4c2b787da327f61d1b1c6bc
Bennett Lin <bennettslin@gmail.com>
2026-06-01 00:52:32 -0700

    Revise richest nations highlight.

--- src/pages/indie/highlights/richest-nations.jsx
+++ src/pages/indie/highlights/richest-nations.jsx
@@ -13,7 +13,7 @@ const Component = () => (
             body: `
 > I can walk into a shop, pick up something expensive, and then walk out […] without handing over any cash; I use a credit card [instead]. The shop assistant doesn't know whether to trust me, but she trusts the credit card company, and they trust me.
 
-> That's a formalized form of trust […] that dramatically expands our ability to interact with those beyond our immediate neighbors. […] The difference between countries that have successfully formalized trust and those that haven't is, basically, the difference between rich countries and poor ones.
+> That's a formalized form of trust, [which] dramatically expands our ability to interact with those beyond our immediate neighbors. [And] the difference between countries that have successfully formalized trust and those that haven't is, basically, the difference between rich countries and poor ones.
 
 Harford, Tim. "[The Stag Hunt.](https://slate.com/culture/2007/09/the-obscure-game-theory-problem-that-explains-why-rich-countries-are-rich.html)" *Slate*, September 2007.
             `,
