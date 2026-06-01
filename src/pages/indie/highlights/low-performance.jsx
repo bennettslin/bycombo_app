5f815592f2cfe414ecb3871bf223550a8afff47c
Bennett Lin <bennettslin@gmail.com>
2026-06-01 02:43:43 -0700

    Revise low performance highlight.

--- src/pages/indie/highlights/low-performance.jsx
+++ src/pages/indie/highlights/low-performance.jsx
@@ -13,7 +13,7 @@ const Component = () => (
             body: `
 > People summon the energy to solve a problem when they sense a gap between their goal and their present state. In a drift to low performance, that gap closes— not because performance improves, but because the goal is allowed to slip. […] The kind of performance that used to cause consternation begins to be defined as normal.
 
-> Drift to low performance feeds upon itself: As everyone gets used to mediocrity, it's easy for standards to slip still further— which [then] lowers expectations and allows even worse performance. […] Once a slide downhill gets going, it's hard to stop, because no one believes what was once possible is possible any more.
+> Drift to low performance feeds upon itself: As everyone gets used to mediocrity, it's easy for standards to slip still further— which [then] lowers expectations and allows even worse performance. […] Once a slide downhill gets going, it's hard to stop, because no one believes that what was once possible is possible any more.
 
 Meadows, Donella. "[Drift to Low Performance.](https://donellameadows.org/archives/drift-to-low-performance)" *The Donella Meadows Project*, August 1991.
             `,
