7f4768cb97809930bc0f91aa231d2e6262dfb73e
Bennett Lin <bennettslin@gmail.com>
2026-06-04 08:53:45 -0700

    Morning review.

--- src/pages/indie/commentary/audience.jsx
+++ src/pages/indie/commentary/audience.jsx
@@ -11,10 +11,6 @@ const Component = () => (
             metaTitle: TITLE,
             metaDescription: COMMENTARY_DESCRIPTION,
             body: `
-${'  ' || `
-// TODO: Review commentary.
-`}
-
 I'd like to push back against the term "[audience capture](https://www.gurwinder.blog/p/the-perils-of-audience-capture)," which has gained some traction recently. Mind you, I'm on board with the underlying concept; I just wish more thought had gone into choosing a better first word! After all, there are plenty of others to describe a crowd whose fickle tastes and selfish demands you should be wary of: "followers," "subscribers"…
 
 By contrast, "audience" is the only word we have to describe those you *must* keep in mind. Once we muddy its meaning, then, we lose the notion entirely that the best art is made in service to others. So let's not muddy it! In saying, "Make art for your audience," I shouldn't face any backlash— other than from those annoyed that I've just stated a tautology.
