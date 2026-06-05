4364a998fd3f68e5edd76ed2b84740477ce89764
Bennett Lin <bennettslin@gmail.com>
2026-06-04 20:47:40 -0700

    Revise cred commentary.

--- src/pages/indie/commentary/cred.jsx
+++ src/pages/indie/commentary/cred.jsx
@@ -15,7 +15,7 @@ ${'  ' || `
 // TODO: Review a few more times.
 `}
 
-As we all know, "cred" is short for the "credibility" you earn when coming up from the streets or the underground… and *not* for the "credentials" sought by academic and corporate strivers. Yet it's no coincidence the two words share a root: They both merely *signify* what matters, no? Which explains why indie rock is now overrun with the same strivers— who live to win by paying dues!
+As we all know, "cred" is short for the "credibility" you earn when coming up from the streets or the underground— and *not* for the "credentials" sought by academic and corporate strivers. Yet it's no coincidence the two words share a root: They both merely *signify* what matters; they both count as paid dues. Which explains why indie rock is now overrun with the same strivers!
             `,
         }}
     />
