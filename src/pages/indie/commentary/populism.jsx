ec24ad7a1b6999d99cb54b205cd9df7c97639a45
Bennett Lin <bennettslin@gmail.com>
2026-06-07 18:10:40 -0700

    Evening review.

--- src/pages/indie/commentary/populism.jsx
+++ src/pages/indie/commentary/populism.jsx
@@ -12,7 +12,7 @@ const Component = () => (
             metaDescription: COMMENTARY_DESCRIPTION,
             body: `
 ${'  ' || `
-// TODO: Review a few more times.
+// TODO: Review.
 `}
 
 Every one of us minorities— whether racial, ethnic, or religious— also lives as a *numerical* minority. So when populists threaten to overpower the elites through sheer numerical might, this threat feels directed at us as well… even if we agree with their policies. After all, whose side will the numbers favor in the *future*? We don't take it as given that we won't be outnumbered again.
