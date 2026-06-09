8992fca3a54e362fb0a48662110e5cf00f0a5844
Bennett Lin <bennettslin@gmail.com>
2026-06-09 09:50:05 -0700

    Morning review and revision.

--- src/pages/indie/commentary/cred.jsx
+++ src/pages/indie/commentary/cred.jsx
@@ -11,7 +11,11 @@ const Component = () => (
             metaTitle: TITLE,
             metaDescription: COMMENTARY_DESCRIPTION,
             body: `
-As we all know, "cred" is short for the "credibility" you earn when coming up from the streets or the underground— and *not* for the "credentials" sought by academic and corporate strivers. Yet it's no coincidence the two words share a root: They both merely *signify* what matters; they both count as paid dues, no? Which explains why indie rock is now overrun with strivers as well!
+${'  ' || `
+// TODO: Review.
+`}
+
+As we all know, "cred" is short for the "credibility" you earn when coming up from the streets or the underground— and *not* for the "credentials" sought by academic and corporate strivers. Yet it's no coincidence the two words share a root: They both merely *signify* what matters; they *both* count as paid dues, no? Which explains why indie rock is now overrun with strivers as well!
             `,
         }}
     />
