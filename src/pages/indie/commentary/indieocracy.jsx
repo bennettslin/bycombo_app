0b6d55ce09728e9444220e1b4ba0d3ee11f3e6af
Bennett Lin <bennettslin@gmail.com>
2026-06-20 13:51:55 -0700

    Revise indieocracy commentary again.

--- src/pages/indie/commentary/indieocracy.jsx
+++ src/pages/indie/commentary/indieocracy.jsx
@@ -15,10 +15,9 @@ ${'  ' || `
 // TODO: Review a few more times.
 `}
 
+What's the fate of a society in which natural selection no longer weeds out those with the worst traits, allowing them to live on and flood our collective gene pool? The movie [*Idiocracy*](https://en.wikipedia.org/wiki/Idiocracy) offers a glimpse: Its hero— an average Joe in our own times— is placed in hibernation and wakes up five hundred years later, only to discover he's now the smartest person alive.
 
-What's the fate of an advanced society in which natural selection no longer punishes those with the worst traits, allowing them to live on and reproduce even further? The movie [*Idiocracy*](https://en.wikipedia.org/wiki/Idiocracy) offers a glimpse: Its hero— an average Joe in our own times— is placed in hibernation and wakes up five hundred years later, only to discover he's now the smartest person alive.
-
-Likewise, in an "indiefied" world, success goes to those who divert the most effort toward *getting* the job, away from simply *deserving* it— and it's their works that then live on to influence the next generation. So what's *our* fate? We could explore this answer in a movie as well… except, of course, instead of a fictional comedy, it'd be a documentary of our times.
+Likewise, thanks to "indiefication," we no longer weed out those who put less effort into making art, allowing them to flood the arts with their works— and it's these works that'll then live on to influence the next generation. So what's *our* fate? We could explore this answer in a movie as well… except, of course, instead of a fictional comedy, it'd be an earnest documentary of our times.
             `,
         }}
     />
