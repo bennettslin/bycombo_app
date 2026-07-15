36c704251b606dc745cd2dd148298ae85dd48c56
Bennett Lin <bennettslin@gmail.com>
2026-07-14 23:52:42 -0700

    Reviews and revision.

--- src/pages/indie/commentary/cred.jsx
+++ src/pages/indie/commentary/cred.jsx
@@ -14,10 +14,10 @@ const Component = () => (
 As we all know, "cred" is short for the "credibility" you earn when coming up from the streets or the underground— and *not* for the "credentials" sought by academic and corporate strivers. Yet it's no coincidence the two words share a root: They both merely *signify* what matters; they *both* count as paid dues, no? Which explains why indie rock is now overrun with strivers as well!
 
 ${'  ' || `
-// TODO: Review a few more times.
+// TODO: Review.
 `}
 
-Here, then, is another reason why "elite capture" is poorly named, as stated in my previous commentary: It keeps us from noticing the same pattern play out in *anti*-elitist cultures like indie rock! So let's call *all* of it "cred"… because, what's the difference between the underground and any formal institution, if both can be "captured" by strivers sending all the right signals?
+Here, then, is another reason why "elite capture" is poorly named, as stated in my previous commentary: It keeps us from noticing the same pattern play out in *anti*-elitist cultures like indie rock! So let's call *all* of it "cred"… Because, what's so different between the underground and any formal institution, if both can be "captured" by strivers sending all the right signals?
             `,
         }}
     />
