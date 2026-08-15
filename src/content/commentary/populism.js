90b9f3b44c2da9cc4f9096053c781937e83a61d7
Bennett Lin <bennettslin@gmail.com>
2026-08-15 14:05:32 -0700

    Revise populism commentary.

--- src/content/commentary/populism.js
+++ src/content/commentary/populism.js
@@ -5,9 +5,9 @@ ${'  ' || `
 // TODO: Review whole commentary a few more times.
 `}
 
-How did indie rock, which had originally *subverted* the people's will, become a populist movement that now *obeys* it instead? After being stumped for so long, I finally understand: Populism doesn't so much rebel against *economic* elites as against *cognitive* ones. That is to say, it trusts the people's common sense and intuition over the elite's abstract and deliberate way of thinking.
+How did indie rock, which had originally *subverted* the people's will, become a populist movement that now *obeys* it instead? I've finally solved the puzzle: Populism doesn't so much rebel against *economic* elites as against *cognitive* ones. That is to say, it favors our human intuition to understand the world in concrete terms, over the elite's way of reasoning through abstract concepts.
 
-Well, as stated earlier, the indie labels are all about conserving brainpower, while indie tastemakers pride themselves on being "in the know"— both of which favor a reliance on gut instincts, as common people do. Meanwhile, because indie rock's snobbery had always been based on exclusivity rather than elitism, it was never loyal to any system of elites in the first place.
+Well, as stated earlier, the indie labels are all about conserving brainpower, while indie tastemakers pride themselves on being "in the know," right? So both rely on gut instincts and concrete metrics, like populists do. And since indie rock's original snobbery had always been based on exclusivity rather than elitism, it was never loyal to any system of elites in the first place.
 
 Heath, Joseph. "[The Populist Revolt Against Cognitive Elites.](https://www.theatlantic.com/ideas/2025/12/populism-left-wing-mamdani/685238)" *The Atlantic*, December 2025.
     `,
