bfd5b1b74c83c29cdd19573a2b4f32880a3ddd36
Bennett Lin <bennettslin@gmail.com>
2026-08-16 00:32:41 -0700

    Revise wokeness commentary.

--- src/content/commentary/logistics.js
+++ src/content/commentary/logistics.js
@@ -2,7 +2,7 @@ export default {
     title: '"logistics"',
     body: `
 ${'  ' || `
-// TODO: Review whole commentary a few more times.
+// TODO: Keep working on.
 `}
 
 Blah.
--- src/content/commentary/wokeness.js
+++ src/content/commentary/wokeness.js
@@ -5,8 +5,10 @@ ${'  ' || `
 // TODO: Review whole commentary a few more times.
 `}
 
-Wokeness blah.
+Having never gotten into social media, I was mostly oblivious to the culture wars of the '10s that led up to peak wokeness in the '20s. Still, everything I've read after the fact has convinced me that wokeness is just indie rock's ideals revamped for women and minorities— because every gripe about it sounds to me like the ones I was making about indie rock in the '00s. Consider:
 
-Wokeness blah.
+- Each movement began with the best of intentions: to pick the underdogs of their times.
+- In doing so, however, each consistently benefited certain demographics over others.
+- Eventually, then, each collapsed under the weight of its own hypocrisy, thus exposing the paradox at the heart of every underdog movement: Once you lift up the underdog… they're no longer an underdog.
     `,
 }
