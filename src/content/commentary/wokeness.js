10c626e9b5b1eedf7cb7cbd3bdb809ce5b002e75
Bennett Lin <bennettslin@gmail.com>
2026-08-17 13:48:07 -0700

    Revise wokeness commentary.

--- src/content/commentary/wokeness.js
+++ src/content/commentary/wokeness.js
@@ -5,7 +5,7 @@ ${'  ' || `
 // TODO: Review.
 `}
 
-Having never gotten into social media, I was mostly oblivious to the culture wars of the '10s that led up to peak wokeness in the '20s. Still, everything I've read after the fact has convinced me that wokeness is just indie rock's ideals revamped for women and minorities— because every gripe about it sounds like the ones I was making about indie rock in the '00s. Consider:
+Having never gotten into social media, I was mostly oblivious to the culture wars of the '10s that led to peak "wokeness" in the '20s. Still, everything I've read after the fact has convinced me that wokeness is just indie rock's ideals revamped for women and minorities— because every gripe about it sounds like the ones I was making about indie rock in the '00s. Consider:
 
 ${'  ' || `
 // TODO: Review.
