975bb46baa8e060bb69e9d787d746b9696e14ace
Bennett Lin <bennettslin@gmail.com>
2026-08-19 21:21:18 -0700

    Revise wokeness commentary.

--- src/content/commentary/wokeness.js
+++ src/content/commentary/wokeness.js
@@ -3,8 +3,12 @@ export default {
     body: `
 Having never gotten into social media, I was mostly oblivious to the culture wars of the '10s that led to peak "wokeness" in the '20s. Still, everything I've read after the fact has convinced me that wokeness is just indie rock's ideals revamped for women and minorities— because every gripe about it sounds like the ones I was making about indie rock in the '00s. Consider:
 
-- Each movement began with the best of intentions— namely, to lift up the underdogs of their times.
-- In doing so, however, each consistently benefited certain demographics over others.
-- Eventually, then, each collapsed under the weight of its own hypocrisy, thus exposing the paradox at the heart of *every* underdog movement: Once you lift up the underdogs… they're no longer underdogs.
+${'  ' || `
+// TODO: Review a few more times.
+`}
+
+- Each movement began with the best of intentions— namely, to lift up the underdogs of their times. In doing so, however, each benefited the same demographics over others, time and time again.
+- Those of us who protested were ignored, mocked, or vilified— and this was seen as justified, given that we'd chosen to take sides *against* good intentions. Still, what we *did* have on our side was reason.
+- Eventually, then, each movement collapsed under the weight of its own hypocrisy, thus revealing the paradox at the heart of *all* good intentions: Once you lift up the underdog… they're no longer an underdog.
     `,
 }
