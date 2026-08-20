7419e88162cefdcbd233284f5b84d360f3d73782
Bennett Lin <bennettslin@gmail.com>
2026-08-19 22:14:00 -0700

    Minor change.

--- src/content/commentary/wokeness.js
+++ src/content/commentary/wokeness.js
@@ -4,7 +4,7 @@ export default {
 Having never gotten into social media, I was mostly oblivious to the culture wars of the '10s that led to peak "wokeness" in the '20s. Still, everything I've read after the fact has convinced me that wokeness is just indie rock's ideals revamped for women and minorities— because every gripe about it sounds like the ones I was making about indie rock in the '00s. Consider:
 
 ${'  ' || `
-// TODO: Review a few more times.
+// TODO: Review a few more times. Does this, with the section, all make sense?
 `}
 
 - Each movement began with the best of intentions— namely, to lift up the underdogs of their times. In doing so, however, each benefited the same demographics over others, time and time again.
--- src/content/reference/place-of-origin.jsx
+++ src/content/reference/place-of-origin.jsx
@@ -1,7 +1,7 @@
 export default {
     title: `"place of origin"`,
     body: `
-> They're from Wisconsin and I'll always cape for Wisco rock.
+> They're from Wisconsin, and I'll always cape for Wisco rock.
 
 Hyden, Steven. "[I interviewed Emily from @slowpulpband…](https://x.com/Steven_Hyden/status/1708868954741977173)" *X*, October 2023.
     `,
