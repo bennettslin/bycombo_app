6f9cb4a0b6c3677a15ae0ace6641666c61e8c6b3
Bennett Lin <bennettslin@gmail.com>
2026-08-22 10:12:42 -0700

    Morning reviews and revisions.

--- src/content/commentary/populism.js
+++ src/content/commentary/populism.js
@@ -2,7 +2,7 @@ export default {
     title: '"populism"',
     body: `
 ${'  ' || `
-// TODO: Review a few more times.
+// TODO: Review.
 `}
 
 How did indie rock, which had originally *subverted* the people's will, become a populist movement that now *obeys* it instead? I've finally solved the puzzle: Populism doesn't so much rebel against *economic* elites as against *cognitive* ones. That is, it trusts the people's snap judgments based on what's plain to see, over the elite's way of reasoning through abstract concepts.
@@ -11,7 +11,7 @@ ${'  ' || `
 // TODO: Review a few more times.
 `}
 
-Well, as stated earlier, the indie labels are all about conserving brainpower, right? So they rely on gut instincts and concrete metrics, just like populists do. And since indie rock's original snobbery had been based on bottom-up exclusivity rather than top-down elitism, it was never loyal to any system of elites in the first place.
+Well, as stated earlier, the indie labels are all about conserving brainpower, right? So they rely on gut instincts and concrete metrics, just like populists do. And since indie rock's snobbery had always been based on bottom-up exclusivity rather than top-down elitism, it was never loyal to any system of elites in the first place.
 
 Heath, Joseph. "[The Populist Revolt Against Cognitive Elites.](https://www.theatlantic.com/ideas/2025/12/populism-left-wing-mamdani/685238)" *The Atlantic*, December 2025.
     `,
--- src/pages/indie/index.jsx
+++ src/pages/indie/index.jsx
@@ -376,7 +376,7 @@ Which explains why the disadvantaged tend to be *put off* by populism: It tells 
 In high school, a classmate referred to the carpetbaggers as "bad guys"— for exploiting the defeated South. This surprised me, since I saw them as "good guys"— for defending the freed slaves. Clearly, he stood on the people's side, while I sided with the elites and the underclass. So this model of a "sideways horseshoe" shows up not just in the arts… but in politics as well!
 
 ${'  ' || `
-// TODO: Review a few more times. Also review link change.
+// TODO: Review.
 `}
 
 Now, our forebears didn't need to coin such a term: They intuitively grasped why the underclass should [shy away](${REFERENCE_PATH}/shy-away#) from populism. But Millennials never developed this intuition; as a result, their progressives keep trying to pit *all* of us against the political establishment… only to [watch in confusion](${REFERENCE_PATH}/watch-in-confusion#) as the disadvantaged among us side with the establishment instead.
@@ -449,11 +449,7 @@ Given their disdain for [well-crafted hooks](${REFERENCE_PATH}/well-crafted-hook
 
 "Wait, indie rock set the stage for poptimism?" Yup! Think about it: Both are underdog movements that replaced an earlier culture founded by outsiders. For the former, it was Sister Rosetta Tharpe's rock and roll; and for the latter, Tin Pan Alley's patchwork of Jewish immigrants. Both are also rooted less in *aesthetics*, than in *ethics*— which lose all meaning if dissent is allowed.
 
-${'  ' || `
-// TODO: Review a few more times.
-`}
-
-And so both protect their chosen underdogs by never once asking: "Wouldn't a hungry outsider— one who tries much harder, to be much better— take us *farther*?" But if an underdog *isn't* the best of our times… then they'll also need history to *remember* them as the underdog. Yet future generations will know of every opportunity they were given that was denied to the outsider!
+And so both protect their chosen underdogs by never once asking: "Wouldn't a hungry outsider— one who tries much harder, to be much better— take us *farther*?" But if an underdog *isn't* the best of our times… then they'll also need history to *remember* them as the underdog. Yet future generations will know of every advantage they were given that was denied to the outsider!
 
 This… *can't* be a viable way to make history. Because, what notable figures from *our* past require the same cognitive dissonance of *us*? I can only think of the Ku Klux Klan and their ilk— America's original underdogs, who fought to suppress America's [original outsiders](${REFERENCE_PATH}/original-outsiders#)! Which doesn't bode well for indie rock's legacy… or poptimism's future…
 
