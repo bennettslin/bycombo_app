3a0fb077aa79bb7e13dbce055740bd32fc395b70
Bennett Lin <bennettslin@gmail.com>
2026-08-22 15:42:21 -0700

    Revise populism commentary again.

--- src/content/commentary/populism.js
+++ src/content/commentary/populism.js
@@ -2,16 +2,16 @@ export default {
     title: '"populism"',
     body: `
 ${'  ' || `
-// TODO: Review.
+// TODO: Review a few more times.
 `}
 
-How did indie rock, which had originally *subverted* the people's will, become a populist movement that now *obeys* it instead? I've finally solved the puzzle: Populism doesn't so much rebel against *economic* elites as against *cognitive* ones. That is, it trusts the people's snap judgments based on what's plain to see, over the elite's way of reasoning through abstract concepts.
+How did indie rock, which had originally *subverted* the people's will, become a populist movement that now *obeys* it instead? I've finally solved the puzzle: Populism doesn't so much rebel against *economic* elites as against *cognitive* ones. That is, it trusts common sense and intuition grounded in what's plain to see, over a more deliberate way of reasoning through abstract concepts.
 
 ${'  ' || `
 // TODO: Review a few more times.
 `}
 
-Well, as stated earlier, the indie labels are all about conserving brainpower, right? So they rely on gut instincts and concrete metrics, just like populists do. And since indie rock's snobbery had always been based on bottom-up exclusivity rather than top-down elitism, it was never loyal to any system of elites in the first place.
+Well, as stated earlier, the indie labels are all about conserving brainpower, while indie tastemakers pride themselves on being "in the know," right? So both rely on gut instincts and concrete metrics, as the people do. And since indie rock's snobbery was always based on exclusivity rather than elitism, it was never loyal to any system of elites in the first place.
 
 Heath, Joseph. "[The Populist Revolt Against Cognitive Elites.](https://www.theatlantic.com/ideas/2025/12/populism-left-wing-mamdani/685238)" *The Atlantic*, December 2025.
     `,
