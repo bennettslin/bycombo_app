1b7e34afe0643fed0a2b0750421453f24d97edb4
Bennett Lin <bennettslin@gmail.com>
2026-08-23 21:15:27 -0700

    Add paragraph to populism commentary.

--- src/content/commentary/populism.js
+++ src/content/commentary/populism.js
@@ -5,7 +5,7 @@ ${'  ' || `
 // TODO: Review a few more times.
 `}
 
-How did indie rock, which had originally *subverted* the people's will, become a populist movement that now *obeys* it instead? I've finally solved the puzzle: Populism doesn't so much rebel against *economic* elites as against *cognitive* ones. That is, it trusts common sense and intuition grounded in what's plain to see, over deliberate reasoning by way of abstract concepts.
+How did indie rock, which had originally *subverted* the people's will, become a populist movement that now *obeys* it instead? I've finally solved the puzzle: Populism doesn't so much rebel against *economic* elites as against *cognitive* ones. That is to say, it trusts common sense and intuition grounded in what's plain to see, over deliberate reasoning by way of abstract concepts.
 
 ${'  ' || `
 // TODO: Review a few more times.
@@ -13,7 +13,11 @@ ${'  ' || `
 
 Well, as stated earlier, the indie labels are all about conserving brainpower, while indie tastemakers pride themselves on being "in the know," right? So both rely on gut instincts and concrete metrics, as the people do. And since indie rock's snobbery was always based on exclusivity rather than elitism, it was never loyal to any system of elites to begin with.
 
+${'  ' || `
+// TODO: Review a few more times. Is "one more point" the right segue?
+`}
 
+One more point: As with gatekeepers, if *all* of us oppose the elites, yet differ wildly on who they even are… then what purpose does the notion serve? Let's agree, then, that the only true elite is a *cognitive* one, as both the artistic and political right see it— since they're the ones who do populism best. In which case, those of us on the left would be better off *embracing* our elitism!
 
 Heath, Joseph. "[The Populist Revolt Against Cognitive Elites.](https://www.theatlantic.com/ideas/2025/12/populism-left-wing-mamdani/685238)" *The Atlantic*, December 2025.
     `,
