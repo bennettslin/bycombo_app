07f23146988f9710710d93000d2d33327bbcf3d4
Bennett Lin <bennettslin@gmail.com>
2026-08-24 15:37:16 -0700

    Revise populism commentary.

--- src/content/commentary/populism.js
+++ src/content/commentary/populism.js
@@ -5,19 +5,19 @@ ${'  ' || `
 // TODO: Review.
 `}
 
-How did indie rock, which had originally *subverted* the people's will, become a populist movement that now *obeys* it instead? I've finally solved the puzzle: Populism doesn't so much rebel against *economic* elites as against *cognitive* ones. That is to say, it trusts common sense and intuition grounded in what's plain to see, over deliberate reasoning by way of abstract concepts.
+How did indie rock, which had originally *subverted* the people's will, become a populist movement that now *obeys* it instead? I've finally solved the puzzle: Populism doesn't so much rebel against *economic* elites as against *cognitive* ones. That is to say, it favors common sense and intuition grounded in what's plain to see, over deliberate reasoning by way of abstract concepts.
 
 ${'  ' || `
-// TODO: Review.
+// TODO: Review a few more times.
 `}
 
-Well, as stated earlier, the indie labels are all about conserving brainpower, while indie tastemakers pride themselves on being "in the know," right? So both rely on gut instincts and concrete metrics, as the people do. And since indie rock's snobbery was always based on exclusivity rather than elitism, it was never loyal to any system of elites to begin with.
+Well, as stated earlier, the indie labels are all about conserving brainpower, right? So they trust concrete metrics, while relying on gut instincts to make sense of them, as the people do. And since indie rock's snobbery was always based on exclusivity rather than elitism, it was never loyal to any system of elites to begin with.
 
 ${'  ' || `
-// TODO: Review a few more times. Is "one more point" the right segue? Don't repeat "one"? Maybe say, "Many of us oppose the elites." "But then, what purpose does the notion serve? So let's agree…"
+// TODO: Review a few more times.
 `}
 
-One more point: As with gatekeepers, if *all* of us oppose the elites, yet differ wildly on who they even *are*… then what purpose does the notion serve? Let's agree, then, that the only true elite is a *cognitive* one, as both the artistic and political right see it— since they're the ones who do populism best. In which case, all of us on the left would be better off *embracing* our elitism!
+Which leads to a final point: As with gatekeepers, if *all* of us oppose the elites, yet differ wildly on who they even *are*… then we're united against nothing! So let's agree, the truest elite is a *cognitive* one, as both the artistic and political right see it— since it's their side that does populism best. In which case, all of us on the left are better off *embracing* our elitism!
 
 Heath, Joseph. "[The Populist Revolt Against Cognitive Elites.](https://www.theatlantic.com/ideas/2025/12/populism-left-wing-mamdani/685238)" *The Atlantic*, December 2025.
     `,
