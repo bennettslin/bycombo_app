3a7312c8d07e968088797bc2b77b8ba9b89b18a6
Bennett Lin <bennettslin@gmail.com>
2026-08-25 00:35:52 -0700

    Real final changes for the night.

--- src/content/commentary/populism.js
+++ src/content/commentary/populism.js
@@ -8,16 +8,16 @@ ${'  ' || `
 How did indie rock, which had originally *subverted* the people's will, become a populist movement that now *obeys* it instead? The answer lies in this claim: Populism doesn't so much rebel against *economic* elites as against *cognitive* ones. That is to say, it favors common sense and intuition grounded in what's plain to see, over deliberate reasoning by way of abstract concepts.
 
 ${'  ' || `
-// TODO: Review a few more times.
+// TODO: Review a few more times. Reiterate that this makes the indie camp populist?
 `}
 
-Well, as stated earlier, the indie labels are all about conserving brainpower, right? So they trust concrete metrics, while relying on gut instincts to make sense of it all, as the people do. And since indie rock's snobbery was always based on exclusivity rather than elitism, it was never loyal to any institution for selecting elites to begin with.
+Well, as stated earlier, the indie labels are all about conserving brainpower, right? So they trust concrete metrics, while relying on gut instincts to make sense of it all, as the people do. And since indie rock's snobbery was always based on exclusivity rather than elitism, it was never loyal to any system of elites to begin with. Which is how indie rock came to embrace populism!
 
 ${'  ' || `
 // TODO: Review a few more times.
 `}
 
-Which leads to a final point: As with gatekeepers, if *all* of us oppose the elites, yet differ wildly on who they even *are*… then we're united against nothing! So let's go with the right's understanding— including those on the *artistic* right, like the indie camp— since they do populism best: The true elite is a *cognitive* one. In turn, this means we on the left are better off *embracing* elitism!
+This leads to my final point: As with gatekeepers, if *all* of us oppose the elites, yet differ wildly on who they even *are*… then we're united against nothing! So let's go with the right's understanding— including those on the *artistic* right, like the indie camp— since they do populism best: The true elite is a *cognitive* one. Which means we on the left are better off embracing elitism!
 
 Heath, Joseph. "[The Populist Revolt Against Cognitive Elites.](https://www.theatlantic.com/ideas/2025/12/populism-left-wing-mamdani/685238)" *The Atlantic*, December 2025.
     `,
