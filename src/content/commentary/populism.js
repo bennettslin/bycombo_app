cd83f8eaf89fd1200c19ecea573e471cbe92c53c
Bennett Lin <bennettslin@gmail.com>
2026-08-24 20:38:07 -0700

    Further changes.

--- src/content/commentary/populism.js
+++ src/content/commentary/populism.js
@@ -11,13 +11,13 @@ ${'  ' || `
 // TODO: Review a few more times.
 `}
 
-Well, as stated earlier, the indie labels are all about conserving brainpower, right? So they trust concrete metrics, while relying on gut instincts to make sense of them, as the people do. And since indie rock's snobbery was always based on exclusivity rather than elitism, it was never loyal to any system of elites to begin with.
+Well, as stated earlier, the indie labels are all about conserving brainpower, right? So they trust concrete metrics, while relying on gut instincts to make sense of it all, as the people do. And since indie rock's snobbery was always based on exclusivity rather than elitism, it was never loyal to any system of elites to begin with.
 
 ${'  ' || `
 // TODO: Review a few more times.
 `}
 
-Which leads to a final point: As with gatekeepers, if *all* of us oppose the elites, yet differ wildly on who they even *are*… then we're united against nothing! So let's agree, the truest elite is a *cognitive* one, as both the artistic and political right see it— since it's their side that does populism best. In which case, all of us on the left are better off *embracing* our elitism!
+Which leads to a final point: As with gatekeepers, if *all* of us oppose the elites, yet differ wildly on who they even *are*… then we're united against nothing! So let's agree, the truest elite is a *cognitive* one— as the artistic and the political right both see it— since it's their side that does populism best. In which case, those of us on the left are better off *embracing* our elitism!
 
 Heath, Joseph. "[The Populist Revolt Against Cognitive Elites.](https://www.theatlantic.com/ideas/2025/12/populism-left-wing-mamdani/685238)" *The Atlantic*, December 2025.
     `,
