72568332af0a9b511f7b7684b7ce4cf4ff582a86
Bennett Lin <bennettslin@gmail.com>
2026-08-30 09:06:46 -0700

    Revise cronyism section.

--- src/content/commentary/alternative.js
+++ src/content/commentary/alternative.js
@@ -5,9 +5,11 @@ ${'  ' || `
 // TODO: Keep working on.
 `}
 
-Blah.
+What's the definitive difference between alternative and indie rock? Whereas "alternative" 
 
 Blah.
+
+Gioia, Ted. "[In 2024, the Tension Between Macroculture and Microculture Will Turn into War.](https://www.honest-broker.com/p/in-2024-the-tension-between-macroculture)" *The Honest Broker*, December 2023.
 
 Marx, W. David. "[Status Struggles: Micro and Macro Culture.](https://culture.ghost.io/links-on-status-and-culture-january-2024/#:~:text=Status%20Struggles,Macro%20Culture)" *Culture: An Owner's Manual*, January 2024.
     `,
