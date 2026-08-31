e3cefd4c8077840d4f547eea7d83bdd32258f431
Bennett Lin <bennettslin@gmail.com>
2026-08-30 23:54:24 -0700

    Keep working on alternative commentary.

--- src/content/commentary/alternative.js
+++ src/content/commentary/alternative.js
@@ -5,17 +5,19 @@ ${'  ' || `
 // TODO: Keep working on.
 `}
 
-At its core, what's the difference between alternative and indie rock? While "alternative" is an *aesthetic* choice, "indie" is a *logistical* stance. I'm not being pedantic: This distinction is key to understanding why the indie takeover of music in the '00s has left us worse off. Again, this will become clear as you read on, but here's a brief taste of what I mean.
+At its core, what's the difference between alternative and indie rock? While "alternative" is an *aesthetic* choice, "indie" is a *logistical* stance. I'm not being pedantic: This distinction is key to understanding why the indie takeover of music in the '00s has left us worse off. Once again, all this will be made clear as you read on, but here's a brief taste of what I mean…
 
-As W. David Marx explains, unlike the macroculture, the microculture isn't a single entity; there are several, of which the counterculture is just one. Well, the Internet has now given us another: the "macro-taste micro"— as typified by MrBeast— which feeds mainstream tastes but goes through independent distribution. In other words, it differs not in aesthetics… but in logistics!
+Unlike the macroculture, there's no single microculture; as explained by W. David Marx, the counterculture is just one of many. And now the Internet has given us one more: the "macro-taste micro"— as typified by MrBeast— which caters to mainstream *tastes*, but goes through independent *distribution*. In other words, it differs not in aesthetics… but in logistics!
 
 This tells us, indie rock was *always* fated to devolve into a macro-taste micro, once the Internet made this possible— and so it did, starting in the early '00s. Once we grasp this, we can read Marx's critique of the macro-taste micro as criticism of indie rock instead. To paraphrase:
 
-- The alternative takeover in the '90s was the last time a counterculture infused the mainstream with new artistic forms and styles. Alternative, then, was rock's last true counterculture.
+- The alternative takeover in the '90s was the last time mainstream rock was infused with new artistic forms and styles. Alternative, then, was rock's last true counterculture. (Talk about how and where these new forms and styles came to be. Avant-garde? Reference Marx.)
 
-- By contrast, this century's indie artists compete directly with the mainstream without being a source of innovation. For this reason, they still struggle for respect. Not just from elites, but from younger generations.
+- By contrast, this century's indie artists are like the MrBeasts of music, in that they compete directly with the mainstream *without* being a source of innovation. As such, they still struggle for respect— not just from the elites, but even from today's young folks. (Point out they share the same tastes.)
 
-- Because many assume there's a single microculture, all those who wish to support the counterculture end up giving goodwill to indie rock. This prevents any true counterculture from taking root, and is one reason for the decline of the arts in the 21st century.
+- Still, since many assume there's a single microculture— which they then equate with the counterculture— they support indie rock, believing it to be that counterculture. Sadly, this prevents a true counterculture from emerging in our times.
+
+- The lack of a true counterculture is one reason for the decline of the arts in the 21st century.
 
 Marx, W. David. "[Status Struggles: Micro and Macro Culture.](https://culture.ghost.io/links-on-status-and-culture-january-2024/#:~:text=Status%20Struggles,Macro%20Culture)" *Culture: An Owner's Manual*, January 2024.
     `,
