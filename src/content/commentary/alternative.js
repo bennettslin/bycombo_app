151511305225df08c6127f91bd283a34d5287064
Bennett Lin <bennettslin@gmail.com>
2026-09-03 14:33:59 -0700

    Final rough version of alternative commentary.

--- src/content/commentary/alternative.js
+++ src/content/commentary/alternative.js
@@ -5,7 +5,7 @@ ${'  ' || `
 // TODO: Review.
 `}
 
-At its core, what's the difference between alternative and indie rock? While "alternative" is a choice in *aesthetics*, "indie" is a stance on *logistics*. I'm not being pedantic here! This distinction is key to understanding why the indie takeover of music in the '00s has led to our present decline. Again, you'll get the full picture by the end, but I can offer an overview for now.
+At its core, what's the difference between alternative and indie rock? While "alternative" is a choice in *aesthetics*, "indie" is a stance on *logistics*. I'm not being pedantic here! This distinction is key to understanding why the indie takeover of music in the '00s has led to our present decline. Again, you'll get the full picture by the very end, but I can offer an overview for now.
 
 ${'  ' || `
 // TODO: Review.
@@ -17,31 +17,31 @@ ${'  ' || `
 // TODO: Review.
 `}
 
-Given that indie rock is defined solely by its stance on *logistics*, then, it was *always* likely to merge with this macro-taste micro— so it did, starting in the early '00s. And now, with our newfound insight, we can read Marx's critique of the macro-taste micro as criticism of indie rock as well. To paraphrase:
+Given that indie rock is defined solely by its stance on *logistics*, then, it was *always* likely to merge with the macro-taste micro— so it did, starting in the early '00s. And now, with this newfound insight, we can read Marx's critique of the macro-taste micro as criticism of indie rock instead. To paraphrase:
 
 ${'  ' || `
 // TODO: Review a few more times.
 `}
 
-- Rock's last *true* counterculture was the alternative that took over in the '90s. By refreshing the mainstream with new *aesthetics*— that is, new forms and styles born from a conscious intent to advance the art form— those '90s alternative bands have our respect to this day.
+- Rock's last *true* counterculture was the alternative that took over in the '90s. By refreshing the mainstream with new *aesthetics*— that is, with new forms and styles— those '90s alternative bands helped to advance the art form in their own time, and thus have our respect to this day.
 
 ${'  ' || `
 // TODO: Review a few more times.
 `}
 
-- By contrast, this century's indie artists are like the MrBeasts of music: They share the mainstream's tastes, and thus compete for its audience directly. But since they *don't* help to advance the art form, they struggle to earn respect once their time is past.
+- By contrast, this century's indie artists are like the MrBeasts of music: Since they share the mainstream's tastes, they compete directly for its audience— and every innovation the indie camp celebrates is one that enables them to do so. However, this means they *aren't* a source of new aesthetics; as such, they're soon forgotten once their time is past.
 
 ${'  ' || `
 // TODO: Review a few more times.
 `}
 
-- Still, many support indie rock, seeing it as the modern equivalent of past countercultures. And yet, such support would otherwise be given to a *true* counterculture, thus preventing the next one from emerging!
+- Still, many support indie rock, treating it as the modern equivalent of past countercultures. But such support would otherwise be given to a *true* counterculture— which is why none has yet emerged!
 
 ${'  ' || `
 // TODO: Review a few more times.
 `}
 
-- This lack of a true counterculture— namely, a movement defined by its aesthetic choices rather than some logistical stance— is the reason the arts have declined in the 21st century.
+- This lack of a true counterculture— namely, a movement defined by its *aesthetic* choices, rather than any *logistical* stance— is the main reason the arts have declined in the 21st century.
 
 Marx, W. David. "[Status Struggles: Micro and Macro Culture.](https://culture.ghost.io/links-on-status-and-culture-january-2024/#:~:text=Status%20Struggles,Macro%20Culture)" *Culture: An Owner's Manual*, January 2024.
     `,
