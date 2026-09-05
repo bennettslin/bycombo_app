75755d5e24892dd14d3b118781e84dc59b3aa309
Bennett Lin <bennettslin@gmail.com>
2026-09-05 14:42:47 -0700

    Fix jankiness upon load.

--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -1,23 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import cx from 'classnames'
+import { CSSTransition } from 'react-transition-group'
 import Wrapper from '../Wrapper'
 import AppFooter from './Footer'
 import './style'
 
-const App = ({ children }) => (
-    <div
-        {...{
-            className: cx(
-                'App',
-                'font__text',
-            ),
-        }}
-    >
-        <Wrapper>{children}</Wrapper>
-        <AppFooter />
-    </div>
-)
+const App = ({ children }) => {
+    const [didMount, setDidMount] = useState(false)
+
+    useEffect(() => {
+        setTimeout(() => setDidMount(true), 0)
+    }, [])
+
+    return (
+        <CSSTransition
+            appear
+            {...{
+                in: didMount,
+                classNames: {
+                    enterDone: 'shown',
+                },
+            }}
+        >
+            <div
+                {...{
+                    className: cx(
+                        'App',
+                        'hidden',
+                        'font__text',
+                    ),
+                }}
+            >
+                <Wrapper>{children}</Wrapper>
+                <AppFooter />
+            </div>
+        </CSSTransition>
+    )
+}
 
 App.propTypes = {
     children: PropTypes.node.isRequired,
