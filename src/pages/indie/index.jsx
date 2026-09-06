053f6d97bfd2820747dc88eed64a87517e29461c
Bennett Lin <bennettslin@gmail.com>
2026-09-06 11:49:24 -0700

    Revise solutions section.

--- src/pages/indie/index.jsx
+++ src/pages/indie/index.jsx
@@ -615,7 +615,7 @@ ${'  ' || `
 // TODO: Review a few more times.
 `}
 
-This notion of an "[upright horseshoe](https://en.wikipedia.org/wiki/Horseshoe_theory)," then, in which anti-elitists on the left align with those on the right… holds true *only* during the "breaking" phase! Once the "building" phase begins, our best chance as progressives is to *work* with our side's elites. Especially since— as the "*sideways* horseshoe" tells us— we can't be sure if all those even *further* below would favor us over them!
+This notion of an "[upright horseshoe](https://en.wikipedia.org/wiki/Horseshoe_theory)," then, in which anti-elitists on the left align with those on the right… holds true *only* during the "breaking" phase. Once the "building" phase begins, our best chance as progressives is to *work* with the left's elites. Especially since— as the "*sideways* horseshoe" reveals— it isn't given that those below us would otherwise take our side over theirs!
 
 Which means we'll [need to quit](${REFERENCE_PATH}/need-to-quit#) "larping" as revolutionaries— that is, to quit equating virtue with *identity*, rather than with *impact*— and instead, master the skills needed to analyze our problems and propose solutions. But how? Beyond acknowledging the messiness of our human nature… we must also appreciate systems in all their complexity.
 
