dbd38b2891f9c3b50831d53e39d3a777d7a31a47
Bennett Lin <bennettslin@gmail.com>
2026-09-11 03:10:11 -0700

    Keep working on horseshoe commentary.

--- src/content/commentary/horseshoe.js
+++ src/content/commentary/horseshoe.js
@@ -3,17 +3,22 @@ export default {
     body: `
 ${'  ' || `
 // TODO: Keep working on.
-`}
 
 You might've noticed: Those who sit at either end of the "upright horseshoe" tend to then pick a spot midway on the "sideways" one— and vice versa. Why? I suspect it's to do with this notion of cognitive elites mentioned in an earlier commentary.
 
+Decide whether I want cognitive elites first, then cognitive misers; or else, upright horseshoe first, then sideways.
+`}
 
+You might notice: Those who lie midway on the "upright horseshoe" tend to then sit at either end of the "sideways" one— and vice versa. Why? I suspect it's to do with this notion of cognitive elites from my earlier commentary on populism.
 
+With the upright horseshoe, certain views tend to contain within themselves an inherent stewardship of the systems that allow them to be freely held. In politics, for instance, this is liberal democracy; in the arts, it's "rockism"— at least, my definition thereof. But this requires complexity and nuance, which both requires you to be a cognitive elite, and also pushes one closer to the center.
 
-In the upright horseshoe, cognitive elites tend to sit in the center, because they need their arguments to contain within themselves the liberal values
+By contrast, the sideways horseshoe exists precisely because the elites at the top are fully aware that those of us at the bottom *aren't* like them. To win us over, then, they're forced to understand what we want by reasoning from first principles, as well as by speaking with us directly. To synthesize these vastly different ways of being necessarily requires higher cognition.
+
+Cognitive misers are, of course, the exact opposite, and indie rock provides a great example.
+
+The populist in the middle takes the mental shortcut by simply assuming their hurdles are the same, and thus helping them helps all those below them as well.
 
 what allows someone to sit at the extreme ends is that their arguments doesn't need to contain within itself a need to sustain the liberal values that allow them to make such arguments to begin with. This frees them from nuance, keeping their arguments simple. Which is attractive to cognitive misers.
-
-By contrast, the sideways horseshoe exists because the cognitive elites at the top fundamentally recognize they aren't like the underclass at the bottom. This forces them to understand what the underclass wants by reasoning from first principles. But the populist in the middle takes the mental shortcut by simply assuming their hurdles are the same, and thus helping them helps all those below them as well.
-    `,
+`,
 }
